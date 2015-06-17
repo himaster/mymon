1149cbec1fcad777fba737ecac4b5f7ee6c58135
Himaster <himaster@inbox.ru>
2015-06-17 12:25:25 +0300

    Webhook.php modification

--- webhook.php
+++ webhook.php
@@ -1,6 +1,6 @@
 <?php
-    putenv('PATH='. getenv('PATH') .':/var/www/netbox.co.mymon/');
-    exec('git pull 2>&1', $output);
+    putenv('PATH='. getenv('PATH') .':/var/www/netbox.co/mymon/');
+    exec('cd /var/www/netbox.co/mymon/ && git pull https://github.com/himaster/mymon.git master 2>&1', $output);
     $fp = fopen('webhook.txt', 'w');
     $text = print_r($_POST, 1);
     fwrite($fp, $text);
