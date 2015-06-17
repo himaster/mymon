bb28fc45db39e34dfbe37b6eaf8d451114ec31a6
Himaster <himaster@inbox.ru>
2015-06-17 15:11:59 +0300

    webhook.php delete from repo

--- webhook.php
+++
@@ -1,5 +0,0 @@
-<?php
-    putenv('PATH='. getenv('PATH') .':/var/www/netbox.co/mymon/');
-    exec('cd /var/www/netbox.co/mymon/ && git pull https://github.com/himaster/mymon.git master 2>&1', $output);
-    exec('cd /var/www/netbox.co/mymon.test/ && git pull https://github.com/himaster/mymon.git dev 2>&1', $output);
-?>
