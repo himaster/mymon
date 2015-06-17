bac1bbfd1aeef51816b886086aa08b3a04074bac
Himaster <himaster@inbox.ru>
2015-06-17 12:14:38 +0300

    webhook.php modify

--- webhook.php
+++ webhook.php
@@ -1,6 +1,9 @@
 <?php
     putenv('PATH='. getenv('PATH') .':/var/www/netbox.co.mymon/');
     exec('git pull 2>&1', $output);
-    print_r($_POST);
+    $fp = fopen(‘webhook.txt’, ‘w’);
+    $text = print_r($_POST, 1);
+    fwrite($fp, $text);
+    fclose($fp);
     var_dump($output);
 ?>
