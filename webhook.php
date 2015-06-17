7379627b1adf7ce553aef09ab290c2d5de5898dd
Himaster <himaster@inbox.ru>
2015-06-17 12:28:24 +0300

    add git pull to test in webhook.php

--- webhook.php
+++ webhook.php
@@ -1,6 +1,7 @@
 <?php
     putenv('PATH='. getenv('PATH') .':/var/www/netbox.co/mymon/');
     exec('cd /var/www/netbox.co/mymon/ && git pull https://github.com/himaster/mymon.git master 2>&1', $output);
+    exec('cd /var/www/netbox.co/mymon.test/ && git pull https://github.com/himaster/mymon.git dev 2>&1', $output);
     $fp = fopen('webhook.txt', 'w');
     $text = print_r($_POST, 1);
     fwrite($fp, $text);
