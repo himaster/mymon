c2854ac559d13953ab6f58908096daeb1b3b503c
Himaster <himaster@inbox.ru>
2015-06-17 12:09:15 +0300

    delete test.php and modify webhook.php

--- test.php
+++
@@ -1,5 +0,0 @@
-<?php
-    putenv('PATH='. getenv('PATH') .':/var/www/netbox.co.mymon/');
-    exec('git pull 2>&1', $output);
-    var_dump($output);
-?>
--- webhook.php
+++ webhook.php
@@ -1,6 +1,6 @@
 <?php
     putenv('PATH='. getenv('PATH') .':/var/www/netbox.co.mymon/');
     exec('git pull 2>&1', $output);
-    print_r($_POST, 1);
+    print_r($_POST);
     var_dump($output);
 ?>
