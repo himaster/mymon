d33b9e273e3c0a0f8f611bef151bac700d389ab9
Himaster <himaster@inbox.ru>
2015-09-15 19:55:00 +0300

    Auth failed message fix

--- server.php
+++ server.php
@@ -18,7 +18,8 @@ if (isset($_GET['serverip']) && isset($_GET['task'])){
 
 	$connection = ssh2_connect($serverip, 22);
 	if (! ssh2_auth_pubkey_file($connection, 'root', '/var/www/netbox.co/mymon/id_rsa.pub', '/var/www/netbox.co/mymon/id_rsa', '')) {
-		die('Public Key Authentication Failed');
+		echo "<font color='red'>***</font>";
+		die();
 	}
 	switch ($task) {
 		case 'la':
