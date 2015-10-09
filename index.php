d1e11f122ed8ee195bbddccc5e055cc29086e34c
Himaster <himaster@inbox.ru>
2015-10-09 16:46:25 +0300

    css fix

--- index.php
+++ index.php
@@ -46,7 +46,7 @@
 		</span>
 	</div>
 
-	<table border="2" align="center" bgcolor="lightgray">
+	<table class="main_table">
         <col span="4">
             <tr>
                 <td width="80"><b>Server</b></td>
