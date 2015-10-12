c42716512b3af96b984180349334093002bd6175
Himaster <himaster@inbox.ru>
2015-10-12 17:17:54 +0300

    fix

--- avt.php
+++ avt.php
@@ -16,9 +16,9 @@ if(isset($_POST['auth_submit']))
 	else
 		echo 'Неправильное имя или пароль';
 }
-elseif (isset($mymon)) {
- 	$login = $mymon['login'];
-	$password = $mymon['password'];
+elseif (isset($_COOKIE["mymon[login]"])) {
+ 	$login = $_COOKIE["mymon[login]"];
+	$password = $_COOKIE["mymon[password]"];
 	$query = "SELECT id, login, password FROM users WHERE login ='{$login}' AND password='{$password}' AND approvied='1' LIMIT 1";
 	$sql = mysql_query($query) or die(mysql_error());
 	if (mysql_num_rows($sql) == 1) {
