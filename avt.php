c0566d7fe9126e852544aadee0fa8fd50e8bafe0
Himaster <himaster@inbox.ru>
2015-10-12 16:57:41 +0300

    fix

--- avt.php
+++ avt.php
@@ -4,7 +4,7 @@ if(isset($_POST['auth_submit']))
 {
 	$login = $_POST['login'];
 	$password = md5($_POST['password']);
-	$query = "SELECT id, login, password FROM users WHERE login ='{$login}' AND password='{$password}' LIMIT 1";
+	$query = "SELECT id, login, password FROM users WHERE login ='{$login}' AND password='{$password}' AND approvied='1' LIMIT 1";
 	$sql = mysql_query($query) or die(mysql_error());
 	if (mysql_num_rows($sql) == 1) {
 		include "header.html";
