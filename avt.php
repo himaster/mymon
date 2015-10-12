98849b74a35fefa8306734e18a30a349de7d816b
Himaster <himaster@inbox.ru>
2015-10-12 16:35:13 +0300

    if fix

--- avt.php
+++ avt.php
@@ -6,10 +6,11 @@ if(isset($_POST['auth_submit']))
 	$password = md5($_POST['password']);
 	$query = "SELECT id, login, password FROM users WHERE login ='{$login}' AND password='{$password}' LIMIT 1";
 	$sql = mysql_query($query) or die(mysql_error());
-	if (mysql_num_rows($sql) == 1)
+	if (mysql_num_rows($sql) == 1) {
 		include "header.html";
     	include "table.php";
     	include "footer.html";
+    }
 	else
 		echo 'Неправильное имя или пароль';
 }
