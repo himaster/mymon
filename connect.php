2c59392203043477ccf46aacd8bd3cff54acb5de
Himaster <himaster@inbox.ru>
2015-10-12 16:16:01 +0300

    auth added

---
+++ connect.php
@@ -0,0 +1,15 @@
+<?php
+$dblocation = "localhost";
+$dbname = "mymon";
+$dbuser = "mymon";
+$dbpasswd = "eiGo7iek";
+$dbcnx = @mysql_connect($dblocation,$dbuser,$dbpasswd);
+if (!$dbcnx) {
+	echo( "<P> В настоящий момент сервер базы данных не доступен, поэтому корректное отображение страницы невозможно. </P>" );
+	exit();
+}
+if (!@mysql_select_db($dbname, $dbcnx)) {
+	echo( "<P> В настоящий момент база данных не доступна, поэтому корректное отображение страницы невозможно. .</P>" );
+	exit();
+}
+?>
