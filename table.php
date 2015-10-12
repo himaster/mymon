2d0d63518205532500bf344dde0b3dbe8349e586
Himaster <himaster@inbox.ru>
2015-10-12 10:49:20 +0300

    ajax revert

--- table.php
+++ table.php
@@ -25,9 +25,60 @@
 		echo "<td><div id='" .$server. "_rep'></div></td>";
 		echo "<td><div id='" .$server. "_500'></div></td>";
 		echo "<td><div id='" .$server. "_elastic'></div></td>";
-
-        include "ajax.php";
-	}
+        #include "ajax.php";
+?>
+        
+<script>
+function show_<?php echo $server; ?>(){
+        $.ajax({
+            url: "server.php?serverip=<?php echo $serverip;?>&task=la",
+            cache: false,
+            success: function(html){
+                $("#<?php echo $server. '_la'; ?>").html(html);
+            },
+            error: function(){
+                $.ajax(this);
+            }
+        });
+    <?php if (isset($db)) { ?>
+        $.ajax({
+            url: "server.php?serverip=<?php echo $serverip;?>&task=rep",
+            cache: false,
+            success: function(html){
+                $("#<?php echo $server. '_rep'; ?>").html(html);
+            },
+            error: function(){
+                $.ajax(this);
+            }
+        });
+    <?php } ?>
+    <?php if ($errs == 1) { ?>
+        $.ajax({
+            url: "server.php?serverip=<?php echo $serverip;?>&task=500",
+            cache: false,
+            success: function(html){
+                    $("#<?php echo $server. '_500'; ?>").html(html);
+            },
+            error: function(){
+                    $.ajax(this);
+            }
+        });
+    <?php } ?>
+    <?php if ($elastic == 1) { ?>
+        $.ajax({
+            url: "server.php?serverip=<?php echo $serverip;?>&task=elastic",
+            cache: false,
+            success: function(html){
+                    $("#<?php echo $server. '_elastic'; ?>").html(html);
+            },
+            error: function(){
+                    $.ajax(this);
+            }
+        });
+    <?php } ?>
+    }
+</script>
+<?php
 	fclose($file);
 ?>
 </table>
