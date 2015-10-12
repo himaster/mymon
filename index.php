ade1e24dbdc42613445895729578bf9c78f081e6
Himaster <himaster@inbox.ru>
2015-10-12 17:31:57 +0300

    fix

--- index.php
+++ index.php
@@ -12,11 +12,26 @@ if (isset($_COOKIE["mymon"])) {
     }
 	else
 		echo 'Неправильное имя или пароль в куках???';
-}
+} 
+elseif(isset($_POST['auth_submit'])) {
+	$login = $_POST['login'];
+	$password = md5($_POST['password']);
+	$query = "SELECT id, login, password FROM users WHERE login ='{$login}' AND password='{$password}' AND approvied='1' LIMIT 1";
+	$sql = mysql_query($query) or die(mysql_error());
+	if (mysql_num_rows($sql) == 1) {
+		setcookie('mymon[login]', $login);
+		setcookie('mymon[password]', $password);
+		include "header.html";
+    	include "table.php";
+    	include "footer.html";
+    }
+	else
+		echo 'Неправильное имя или пароль';
+} 
 else {
 ?> 
 <table>
-	<form action="avt.php" method="POST">
+	<form action="index.php" method="POST">
 		<tr>
 			<td>Имя</td>
 			<td><input type="text" name="login"></td>
