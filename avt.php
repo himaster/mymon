4fa09617779db372210e1fa3bad1892e5db10df4
Himaster <himaster@inbox.ru>
2015-10-12 17:13:33 +0300

    fix

--- avt.php
+++ avt.php
@@ -7,6 +7,8 @@ if(isset($_POST['auth_submit']))
 	$query = "SELECT id, login, password FROM users WHERE login ='{$login}' AND password='{$password}' AND approvied='1' LIMIT 1";
 	$sql = mysql_query($query) or die(mysql_error());
 	if (mysql_num_rows($sql) == 1) {
+		setcookie('mymon[login]', $login);
+		setcookie('mymon[password]', $password);
 		include "header.html";
     	include "table.php";
     	include "footer.html";
@@ -14,6 +16,19 @@ if(isset($_POST['auth_submit']))
 	else
 		echo 'Неправильное имя или пароль';
 }
+elseif (isset($mymon)) {
+ 	$login = $mymon['login'];
+	$password = $mymon['password'];
+	$query = "SELECT id, login, password FROM users WHERE login ='{$login}' AND password='{$password}' AND approvied='1' LIMIT 1";
+	$sql = mysql_query($query) or die(mysql_error());
+	if (mysql_num_rows($sql) == 1) {
+		include "header.html";
+    	include "table.php";
+    	include "footer.html";
+    }
+	else
+		echo 'Неправильное имя или пароль';
+ } 
 else {
 	header("Location: http://netbox.co/mymon/");
 	die();
