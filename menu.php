d3c63467aa9e67c9d2ea909f7afda5fc270abc63
Himaster <himaster@inbox.ru>
2015-10-13 18:34:52 +0300

    test

--- menu.php
+++ menu.php
@@ -11,11 +11,11 @@
 	<?php
 		echo "<form action=\"reg.php\" method=\"POST\">";
 		echo "<input type=\"text\" name=\"login\" value=\"".$_COOKIE["mymon"]["login"]."\" readonly>";
-		echo "<p><input type=\"password\" name=\"password\" id=\"password\" style=\"display: none; \">";
-		echo "<p><input type=\"password\" name=\"password2\" id=\"password2\" style=\"display: none; \">";
+		echo "<p><input type=\"password\" id=\"password\" style=\"display: none; \">";
+		echo "<p><input type=\"password\" id=\"password2\" style=\"display: none; \">";
 		echo "<p><input type=\"text\" name=\"email\" style=\"display: none; \" value=\"".mysql_fetch_assoc($sql)["email"]."\">";
 		echo "<p><input type=\"submit\" name=\"submit\" style=\"display: none; \">";
-		echo "<p><a href='javascript: document.getElementById(\"password\").style.display=\"block\";'>Edit profile</a>";
+		echo "<p><a href=\"javascript: toggle_visibility('password')\">Edit profile</a>";
 		echo "<p>IP: ".$_SERVER['REMOTE_ADDR']."</p>";
 		echo "<p><a href='http://". $_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']) ."?task=exit'>logout</a>";
 		echo "</form>";
