42bdaedae4930e30378da4bc5f1208cf225e785e
Himaster <himaster@inbox.ru>
2015-10-13 11:19:17 +0300

    test

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ if (isset($_COOKIE["mymon"])) {
 					echo("<div class=\"500err_menu\">");
 					echo("<a href=\"#\" onclick=\"self.close()\">");
 					echo("<img src=\"./images/back.png\"></a>");
-					echo("</div><div class=\"500err\">");
+					echo("</div><div id=\"500err\">");
 
 					$connection1 = ssh2_connect($_GET["serverip"], 22);
 					if (! ssh2_auth_pubkey_file($connection1, 'root', '/var/www/netbox.co/mymon/id_rsa.pub', '/var/www/netbox.co/mymon/id_rsa', '')) {
