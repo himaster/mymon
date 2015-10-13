94b800bc926606ae9b0bf88872c6987f75a35f0f
Himaster <himaster@inbox.ru>
2015-10-13 15:31:29 +0300

    connection test

--- index.php
+++ index.php
@@ -15,6 +15,12 @@ if (isset($_COOKIE["mymon"])) {
 	$query = "SELECT id, login, password FROM users WHERE login ='{$login}' AND password='{$password}' AND approvied='1' LIMIT 1";
 	$sql = mysql_query($query) or die(mysql_error());
 	if (mysql_num_rows($sql) == 1) {
+		if (isset($_GET["serverip"])) {
+			$connection = ssh2_connect($_GET["serverip"], 22);
+			if (! ssh2_auth_pubkey_file($connection, 'root', '/var/www/netbox.co/mymon/id_rsa.pub', '/var/www/netbox.co/mymon/id_rsa', '')) {
+   				die('Public Key Authentication Failed');
+			}
+		}
 		#if (isset($_GET["task"])) {
 			switch ($_GET["task"]) {
 				case "500err":
@@ -26,18 +32,12 @@ if (isset($_COOKIE["mymon"])) {
 					echo("<a href=\"#\" onclick=\"self.close()\">");
 					echo("<img src=\"./images/back.png\"></a>");
 					echo("</div><div class=\"textstyle\">");
-
-					$connection = ssh2_connect($_GET["serverip"], 22);
-					if (! ssh2_auth_pubkey_file($connection, 'root', '/var/www/netbox.co/mymon/id_rsa.pub', '/var/www/netbox.co/mymon/id_rsa', '')) {
-   						die('Public Key Authentication Failed');
-					}
 					$stream1 = ssh2_exec($connection, "cat /var/log/500.errs");
 					stream_set_blocking($stream1, true);
 					$str = stream_get_contents($stream1);
 					echo nl2br($str);
 					echo "</div>";
 					include "footer.html";
-					unset($connection);
 					break;
 
 				case "editor":
@@ -74,11 +74,6 @@ if (isset($_COOKIE["mymon"])) {
     				}
     				$masterip = "88.198.182.130";
 				    $backin = array("88.198.182.132","88.198.182.134","88.198.182.146");
-
-				    $connection = ssh2_connect($masterip, 22);
-				    if (! ssh2_auth_pubkey_file($connection, 'root', '/var/www/netbox.co/mymon/id_rsa.pub', '/var/www/netbox.co/mymon/id_rsa', '')) {
-					   die('Public Key Authentication Failed');
-				    }
 				    $stream = ssh2_exec($connection, "mysql -N -e 'show master status;' | awk '{print $1}'");
 				    stream_set_blocking($stream, true);
 				    $file = stream_get_contents($stream);
@@ -99,7 +94,6 @@ if (isset($_COOKIE["mymon"])) {
 				    $stream = ssh2_exec($connection, "mysql -N -e 'stop slave;'");
 				    $stream = ssh2_exec($connection, "mysql -N -e '$query' 2>&1");
 				    ssh2_exec($connection, "mysql -N -e 'start slave;'");
-				    unset($connection);
 				    break;
 
 				case "top":
@@ -111,16 +105,11 @@ if (isset($_COOKIE["mymon"])) {
 					echo "<a href=\"#\" onclick=\"self.close()\"><img src=\"images/back.png\"></a>";
 					echo "</div><div class=\"textstyle\">";
 					$serverip = $_GET['serverip'];
-					$connection = ssh2_connect($serverip, 22);
-					if (! ssh2_auth_pubkey_file($connection, 'root', '/var/www/netbox.co/mymon/id_rsa.pub', '/var/www/netbox.co/mymon/id_rsa', '')) {
-						die('Public Key Authentication Failed');
-					}
 					$stream = ssh2_exec($connection, "ps aux --sort=-pcpu | head -n 30");
 					stream_set_blocking($stream, true);
 					$str = stream_get_contents($stream);
 					$str = nl2br($str);
 					echo($str);
-					unset($connection);
 					echo "</div>";
 					include "footer.html";
 					break;
@@ -131,7 +120,10 @@ if (isset($_COOKIE["mymon"])) {
 			    	include "footer.html";
 			    	break;
 			}
-		#}		
+		#}
+		if (isset($connection)) {
+			unset($connection);
+		}		
     }
 	else
 		echo 'Неправильное имя или пароль в куках???';
