f6bad19f75ce14b31ad3b3b722967ce6d2924528
Himaster <himaster@inbox.ru>
2015-10-13 18:12:27 +0300

    fix

--- menu.php
+++ menu.php
@@ -11,9 +11,9 @@
 	<?php
 		echo "<form action=\"reg.php\" method=\"POST\">";
 		echo "<input type=\"text\" name=\"login\" value=\"".$_COOKIE["mymon"]["login"]."\" readonly>";
-		echo "<p><input type=\"password\" name=\"password\" style=\"visibility: hidden; \">";
-		echo "<p><input type=\"password\" name=\"password2\" style=\"visibility: hidden; \">";
-		echo "<p><input type=\"text\" name=\"email\" style=\"visibility: hidden; \" value=\"".mysql_fetch_assoc($sql)["email"]."\">";
+		echo "<p><input type=\"password\" name=\"password\" style=\"display: none; \">";
+		echo "<p><input type=\"password\" name=\"password2\" style=\"display: none; \">";
+		echo "<p><input type=\"text\" name=\"email\" style=\"display: none; \" value=\"".mysql_fetch_assoc($sql)["email"]."\">";
 		echo "<p><input type=\"submit\" name=\"submit\">";
 		echo "<p>Edit profile";
 		echo "<p>IP: ".$_SERVER['REMOTE_ADDR']."</p>";
