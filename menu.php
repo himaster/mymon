2e4da48f05cb358741cf4661c33bddf5f7ae8be9
Himaster <himaster@inbox.ru>
2015-10-13 18:50:48 +0300

    test

--- menu.php
+++ menu.php
@@ -10,12 +10,12 @@
 	<b>Profile</b><p>
 	<?php
 		echo "<form action=\"reg.php\" method=\"POST\">";
-		echo "<input type=\"text\" name=\"login\" value=\"".$_COOKIE["mymon"]["login"]."\" readonly>";
-		echo "<p><input type=\"password\" id=\"password\" style=\"display: none; \">";
-		echo "<p><input type=\"password\" id=\"password2\" style=\"display: none; \">";
-		echo "<p><input type=\"text\" id=\"email\" style=\"display: none; \" value=\"".mysql_fetch_assoc($sql)["email"]."\">";
-		echo "<p><input type=\"submit\" id=\"submit\" style=\"display: none; \">";
-		echo "<p><a href=\"javascript: toggle_visibility('password'); toggle_visibility('password2'); toggle_visibility('email'); toggle_visibility('submit');\">Edit profile</a>";
+		echo "<input type=\"text\" id=\"login\" name=\"login\" value=\"".$_COOKIE["mymon"]["login"]."\" readonly>";
+		echo "<p><input type=\"password\" id=\"password\" name=\"password\" style=\"display: none; \">";
+		echo "<p><input type=\"password\" id=\"password2\" name=\"password2\" style=\"display: none; \">";
+		echo "<p><input type=\"text\" id=\"email\" name=\"email\" style=\"display: none; \" value=\"".mysql_fetch_assoc($sql)["email"]."\">";
+		echo "<p><input type=\"submit\" id=\"submit_edit\" name=\"submit_edit\" style=\"display: none; \">";
+		echo "<p><a href=\"javascript: toggle_visibility('password'); toggle_visibility('password2'); toggle_visibility('email'); toggle_visibility('submit_edit');\">Edit profile</a>";
 		echo "<p>IP: ".$_SERVER['REMOTE_ADDR']."</p>";
 		echo "<p><a href='http://". $_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']) ."?task=exit'>logout</a>";
 		echo "</form>";
