ac3f11365f18a517678c69425a7628d093acdfb7
Himaster <himaster@inbox.ru>
2015-10-13 10:27:55 +0300

    500err style->css

--- 500errs.php
+++ 500errs.php
@@ -9,23 +9,18 @@ if (isset($_GET['serverip'])) {
 <head>
     <title>500 Errors of <?php echo $serverip; ?></title>
 
-    <style>
-	.menu {
-	    position: fixed;
-	}
-    </style>
     <link rel="icon" href="http://<?php echo $_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']).$_SERVER['REQUEST_URI']; ?>/favicon.ico" type="image/x-icon">
     <link rel="shortcut icon" href="http://<?php echo $_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']).$_SERVER['REQUEST_URI']; ?>/favicon.ico" type="image/x-icon">
 </head>
 
 <body bgcolor="black" text="lightgray" style="margin: 20;">
-	<div style="position: fixed; z-index: 9999; width: 30px; height: 200px; overflow: hidden; left: 0px; top: 20px;">
+	<div class="500err">
 		<a href="#" onclick="self.close()"><img src="./images/back.png"></a>
 	</div>
 <?php
     $connection1 = ssh2_connect($serverip, 22);
     if (! ssh2_auth_pubkey_file($connection1, 'root', '/var/www/netbox.co/mymon/id_rsa.pub', '/var/www/netbox.co/mymon/id_rsa', '')) {
-	die('Public Key Authentication Failed');
+	   die('Public Key Authentication Failed');
     }
     $stream1 = ssh2_exec($connection1, "cat /var/log/500.errs");
     stream_set_blocking($stream1, true);
