65bab2ca687fa0215b707284b869523e051f5530
Himaster <himaster@inbox.ru>
2015-10-16 13:12:37 +0300

    html fix

--- register.php
+++ register.php
@@ -1,29 +1,31 @@
 <?php
 	include "header.html";
 ?>
-<table class="register">
-	<form action="reg.php" method="POST">
-		<tr>
-			<td>Имя</td>
-			<td><input type="text" name="login" ></td>
-		</tr>
-		<tr>
-			<td>Пароль</td>
-			<td><input type="password" name="password" ></td>
-		</tr>
-		<tr>
-			<td>Повторите пароль</td>
-			<td><input type="password" name="password2"></td>
-		</tr>
-		<tr>
-			<td>Email</td>
-			<td><input type="text" name="email"></td>
-		</tr>
-		<tr>
-			<p><td colspan="2"><input type="submit" value="OK" name="submit" ></td>
-		</tr>
-	</form>
-</table>
+<div class="register">
+	<table>
+		<form action="reg.php" method="POST">
+			<tr>
+				<td>Имя</td>
+				<td><input type="text" name="login" ></td>
+			</tr>
+			<tr>
+				<td>Пароль</td>
+				<td><input type="password" name="password" ></td>
+			</tr>
+			<tr>
+				<td>Повторите пароль</td>
+				<td><input type="password" name="password2"></td>
+			</tr>
+			<tr>
+				<td>Email</td>
+				<td><input type="text" name="email"></td>
+			</tr>
+			<tr>
+				<p><td colspan="2"><input type="submit" value="OK" name="submit" ></td>
+			</tr>
+		</form>
+	</table>
+</div>
 <?php
 	include "footer.html";
 ?>
