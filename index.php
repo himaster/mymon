c944ce45a7c171b196b2c09956eb32a1a2304558
Himaster <himaster@inbox.ru>
2015-10-16 15:27:21 +0300

    exit test

--- index.php
+++ index.php
@@ -18,8 +18,6 @@ if (isset($_COOKIE["mymon"])) {
 	$query = "SELECT id, login, password, email FROM users WHERE login ='{$login}' AND password='{$password}' AND approvied='1' LIMIT 1";
 	$sql = mysql_query($query) or die(mysql_error());
 	if (mysql_num_rows($sql) == 1) {
-		setcookie('mymon[login]', $login, time()+604800);
-		setcookie('mymon[password]', $password, time()+604800);
 		if (isset($_GET["serverip"])) {
 			$connection = ssh2_connect($_GET["serverip"], 22);
 			if (! ssh2_auth_pubkey_file($connection, 'root', '/var/www/netbox.co/mymon/id_rsa.pub', '/var/www/netbox.co/mymon/id_rsa', '')) {
@@ -200,6 +198,8 @@ if (isset($_COOKIE["mymon"])) {
 				break;
 
 			default:
+				setcookie('mymon[login]', $login, time()+604800);
+				setcookie('mymon[password]', $password, time()+604800);
 				include "header.html";
 		    	include "table.php";
 		    	include "footer.html";
