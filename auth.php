54ddba920b0de644ca24086cfbcf830a6eca286d
Himaster <himaster@inbox.ru>
2015-10-16 14:55:15 +0300

    auth fix

--- auth.php
+++ auth.php
@@ -1,21 +1,23 @@
 <?php
 	include "header.html";
 ?>
-<table>
-	<form action="index.php" method="POST">
-		<tr>
-			<td>Имя</td>
-			<td><input type="text" name="login"></td>
-		</tr>
-		<tr>
-			<td>Пароль</td>
-			<td><input type="password" name="password"></td>
-		</tr>
-		<tr>
-			<td colspan="2"> <input type="submit" value="OK" name="auth_submit"></td>
-		</tr>
-	</form>
-</table>
+<div class="register">
+	<table>
+		<form action="index.php" method="POST">
+			<tr>
+				<td>Имя</td>
+				<td><input type="text" name="login"></td>
+			</tr>
+			<tr>
+				<td>Пароль</td>
+				<td><input type="password" name="password"></td>
+			</tr>
+			<tr>
+				<td colspan="2"> <input type="submit" value="OK" name="auth_submit"></td>
+			</tr>
+		</form>
+	</table>
+</div>
 <a href="register.php">Регистрация</a>
 <?php
 	include "footer.html";
