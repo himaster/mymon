3d628034562fc6da3e40e730d2ebaa4c06323f14
Himaster <himaster@inbox.ru>
2015-10-16 15:36:33 +0300

    menu test

--- menu.php
+++ menu.php
@@ -10,10 +10,10 @@
 		<form action="reg.php" method="POST">
 			Username:<input class="username" type="text" id="login" name="login" value="<?php echo $login ?>" readonly>
 			<p><div id="password"  style="display: none;"> Password:<input type="password" /></div>
-			<p>Repeat:<input type="password" id="password2" name="password2" style="display: none;">
-			<p>E-Mail:<input type="text" id="email" name="email" style="display: none;" value="<?php echo mysql_fetch_assoc($sql)['email'] ?>">
-			<p><input type="submit" id="submit_edit" name="submit_edit" style="display: none; ">
-			<p><a href="javascript: toggle_visibility('password'); toggle_visibility('password2'); toggle_visibility('email'); toggle_visibility('submit_edit');">Edit profile</a>
+			<p><div id="password2"  style="display: none;"> Repeat:<input type="password" /></div>
+			<p><div id="email"  style="display: none;"> E-Mail:<input type="text" value="<?php echo mysql_fetch_assoc($sql)['email'] ?>" /></div>
+			<p><div id="submit" style="display: none; "><input type="submit" id="submit_edit" name="submit_edit" /></div>
+			<p><a href="javascript: toggle_visibility('password'); toggle_visibility('password2'); toggle_visibility('email'); toggle_visibility('submit');">Edit profile</a>
 			<p>IP: <?php echo $_SERVER['REMOTE_ADDR'] ?></p>
 			<p><a href="http://<?php echo $_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']) ?>/?task=exit">logout</a>
 		</form>
