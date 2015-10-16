e67be8f26f5c49afcced38a8e2b0fb7eecadef18
Himaster <himaster@inbox.ru>
2015-10-16 15:29:58 +0300

    menu fix fields comments

--- menu.php
+++ menu.php
@@ -8,10 +8,10 @@
 	
 		<b>Profile</b><p>
 		<form action="reg.php" method="POST">
-			<input class="username" type="text" id="login" name="login" value="<?php echo $login ?>" readonly>
-			<p><input type="password" id="password" name="password" style="display: none;">
-			<p><input type="password" id="password2" name="password2" style="display: none;">
-			<p><input type="text" id="email" name="email" style="display: none;" value="<?php echo mysql_fetch_assoc($sql)['email'] ?>">
+			Username:<input class="username" type="text" id="login" name="login" value="<?php echo $login ?>" readonly>
+			<p>Password:<input type="password" id="password" name="password" style="display: none;">
+			<p>Repeat:<input type="password" id="password2" name="password2" style="display: none;">
+			<p>E-Mail:<input type="text" id="email" name="email" style="display: none;" value="<?php echo mysql_fetch_assoc($sql)['email'] ?>">
 			<p><input type="submit" id="submit_edit" name="submit_edit" style="display: none; ">
 			<p><a href="javascript: toggle_visibility('password'); toggle_visibility('password2'); toggle_visibility('email'); toggle_visibility('submit_edit');">Edit profile</a>
 			<p>IP: <?php echo $_SERVER['REMOTE_ADDR'] ?></p>
