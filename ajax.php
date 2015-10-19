82594255da18efb8c188923704312cf7ad0993c1
Himaster <himaster@inbox.ru>
2015-10-19 18:26:46 +0300

    js fix

--- ajax.php
+++ ajax.php
@@ -1,5 +1,5 @@
 <script>
-    $(document).ready(function(){
+    function caller_<?php echo $server; ?>(){
         show("<?php echo $serverip; ?>", "<?php echo $server; ?>", "la");
         <?php if (isset($db)) { ?>
             show("<?php echo $serverip; ?>", "<?php echo $server; ?>", "rep");
@@ -9,7 +9,11 @@
         <?php } ?>
         <?php if ($elastic == 1) { ?>
             show("<?php echo $serverip; ?>", "<?php echo $server; ?>", "elastic");
-        <?php } ?>    
-        //setInterval('show("<?php echo $serverip; ?>", "<?php echo $server; ?>")',15000);
+        <?php } ?>
+    }
+
+    $(document).ready(function(){
+        caller<?php echo $server; ?>();
+        setInterval('caller<?php echo $server; ?>()',15000);
     });
 </script>
