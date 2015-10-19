b59b44445cda6309010739fd30ec9a148c466916
Himaster <himaster@inbox.ru>
2015-10-19 10:51:28 +0300

    css -> style.css

--- table.php
+++ table.php
@@ -1,7 +1,7 @@
 <?php 
     include "menu.php";
 ?>
-<table border="2" align="center" bgcolor="lightgray" class="main_table">
+<table class="main_table">
         <col span="4">
             <tr>
                 <td><b>Server</b></td>
