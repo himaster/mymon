6a772730fc2952c5da696a763c8c506cd349a948
Himaster <himaster@inbox.ru>
2015-10-20 11:58:08 +0300

    breakpoint

--- index.php
+++ index.php
@@ -243,6 +243,7 @@ if (isset($_COOKIE["mymon"])) {
 				break;
 
 			default:
+				echo isset($_SERVER["HTTPS"]);
 				setcookie('mymon[login]', $login, time()+604800, dirname($_SERVER['PHP_SELF']), $_SERVER['HTTP_HOST'], isset($_SERVER["HTTPS"]), true);
 				setcookie('mymon[password]', $password, time()+604800, dirname($_SERVER['PHP_SELF']), $_SERVER['HTTP_HOST'], isset($_SERVER["HTTPS"]), true);
 				include "header.html";
