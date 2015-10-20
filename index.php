181b97e7a734f3b1d70a6a746ea1fb9b476fe3b0
Himaster <himaster@inbox.ru>
2015-10-20 11:52:58 +0300

    test

--- index.php
+++ index.php
@@ -243,7 +243,7 @@ if (isset($_COOKIE["mymon"])) {
 				break;
 
 			default:
-				die(dirname($_SERVER['PHP_SELF']$_SERVER['HTTP_HOST']);
+				die(dirname($_SERVER['PHP_SELF']).$_SERVER['HTTP_HOST']);
 				setcookie('mymon[login]', $login, time()+604800, dirname($_SERVER['PHP_SELF'], $_SERVER['HTTP_HOST'], isset($_SERVER["HTTPS"]), true);
 				setcookie('mymon[password]', $password, time()+604800, dirname($_SERVER['PHP_SELF'], $_SERVER['HTTP_HOST'], isset($_SERVER["HTTPS"]), true);
 				include "header.html";
