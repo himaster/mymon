5a2a53bd61aa0bfad22042ddbab51abd36dda015
Himaster <himaster@inbox.ru>
2015-10-21 21:55:27 +0300

    fix

--- collector.php
+++ collector.php
@@ -15,15 +15,6 @@ if (!mysqli_select_db($connection, $dbname)) {
 	echo( "<P> В настоящий момент база данных не доступна, поэтому корректное отображение страницы невозможно. .</P>" );
 	exit();
 }
-$connection = mysqli_connect($dblocation,$dbuser,$dbpasswd);
-if (!$connection) {
-	echo( "<P> В настоящий момент сервер базы данных не доступен, поэтому корректное отображение страницы невозможно. </P>" );
-	exit();
-}
-if (!mysqli_select_db($connection, $dbname)) {
-	echo( "<P> В настоящий момент база данных не доступна, поэтому корректное отображение страницы невозможно. .</P>" );
-	exit();
-}
 
 
 $servername = "mymon.pkwteile.de";
@@ -54,10 +45,9 @@ while (true) {
 		}
 	}
 	mysqli_free_result($result);
-	mysqli_close($connection);
-	unset($connection);
-
+	unset($result);
 }
+
 return(0);
 
 
