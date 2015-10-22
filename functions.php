9329df1ddb390467634c7077ffffa67a6e437f16
Himaster <himaster@inbox.ru>
2015-10-22 14:30:14 +0300

    test

--- functions.php
+++ functions.php
@@ -25,7 +25,7 @@ function ssh2_return( $connection, $query ) {
 	stream_set_blocking( $stream, TRUE );
 	$output = stream_get_contents( $stream );
 
-	if (!empty($error_output)) return "Timeout";
+	if (!empty($error_output)) return $error_output;
 	else return $output;
 }
 ?>
