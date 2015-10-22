d59a01f4aab9f91b1346d4bd9a98474bf73cf48e
Himaster <himaster@inbox.ru>
2015-10-22 12:58:39 +0300

    test

--- index.php
+++ index.php
@@ -170,6 +170,7 @@ elseif(isset($_POST['auth_submit'])) {
 		setcookie('mymon[login]', $login, time()+604800, dirname($_SERVER['PHP_SELF']), $_SERVER['HTTP_HOST'], isset($_SERVER["HTTP_X_FORWARDED_PROTOCOL"]), true);
 		setcookie('mymon[password]', $password, time()+604800, dirname($_SERVER['PHP_SELF']), $_SERVER['HTTP_HOST'], isset($_SERVER["HTTP_X_FORWARDED_PROTOCOL"]), true);
 		include "header.html";
+		die("test");
     	include "table.php";
     	include "footer.html";
     }
