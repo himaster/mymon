93a58f3bf30ddba4b022a2bec40f38af1a06b2a2
Himaster <himaster@inbox.ru>
2015-11-02 17:55:59 +0200

    trim added

--- editor.php
+++ editor.php
@@ -8,11 +8,11 @@ while ($row_user = $result->fetch_assoc()) {
 ?>
 	<tr>
 		<td><input value="<?php echo trim($row_user['id']) ?>"></td>
-		<td><input value="<?php echo $row_user['ip'] ?>"></td>
-		<td><input value="<?php echo $row_user['servername'] ?>"></td>
-		<td><input value="<?php echo $row_user['db'] ?>"></td>
-		<td><input value="<?php echo $row_user['err'] ?>"></td>
-		<td><input value="<?php echo $row_user['el'] ?>"></td>
+		<td><input value="<?php echo trim($row_user['ip']) ?>"></td>
+		<td><input value="<?php echo trim($row_user['servername']) ?>"></td>
+		<td><input value="<?php echo trim($row_user['db']) ?>"></td>
+		<td><input value="<?php echo trim($row_user['err']) ?>"></td>
+		<td><input value="<?php echo trim($row_user['el']) ?>"></td>
 	</tr>
 <?php
 }
