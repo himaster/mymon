a8697c0232d7be9bfeedbbe3845ff459dcc6a582
Himaster <himaster@inbox.ru>
2015-11-02 15:28:49 +0200

    test

--- collector.php
+++ collector.php
@@ -2,18 +2,12 @@
 
 <?php
 include_once "functions.php";
-
 declare(ticks=1);
-
 set_error_handler('errHandler');
-
 pcntl_signal(SIGTERM, "sigHandler");
-
 $connection = new mysqli("188.138.234.38", "mymon", "eiGo7iek", "mymon") or die($connection->connect_errno."\n");
 $result = $connection->query("SELECT ip, servername, db, err, el FROM `mymon`.`stats`;") or die($connection->error);
-
 $i = 1;
-
 while($array = $result->fetch_assoc()) {
     $pid = pcntl_fork();
     if ($pid == -1) die("Child process can't be created");
@@ -38,9 +32,7 @@ function child_() {
 	global $array;
 	global $stop_server;
 	global $servername;
-
 	$connection1 = new mysqli("188.138.234.38", "mymon", "eiGo7iek", "mymon") or die($connection->connect_errno."\n");
-
 	$serverip = $array["ip"];
 	$servername = $array["servername"];
 	$errs = $array["err"];
@@ -50,23 +42,18 @@ function child_() {
 	while (!$stop_server) {
 		$result = $connection1->query("UPDATE `mymon`.`stats` SET la='" .runtask("la", $serverip). "' WHERE ip='" .$serverip. "';");
 		if (!$result) common_log($servername." - LA not updated!");
-
 		if ($db == 1) $result = $connection1->query("UPDATE `mymon`.`stats` SET rep='" .runtask("rep", $serverip). "' WHERE ip='" .$serverip. "';");
 		else $result = $connection1->query("UPDATE `mymon`.`stats` SET rep='' WHERE ip='" .$serverip. "';");
 		if (!$result) common_log($servername." - REP not updated!");
-
 		if ($errs == 1) $result = $connection1->query("UPDATE `mymon`.`stats` SET `500`='" .runtask("500", $serverip). "' WHERE ip='" .$serverip. "';");
 		else $result = $connection1->query("UPDATE `mymon`.`stats` SET `500`='' WHERE ip='" .$serverip. "';");
 		if (!$result) common_log($servername." - 500 not updated!");
-
 		if ($elastic == 1) $result = $connection1->query("UPDATE `mymon`.`stats` SET elastic='" .runtask("elastic", $serverip). "' WHERE ip='" .$serverip. "';");
 		else $result = $connection1->query("UPDATE `mymon`.`stats` SET elastic='' WHERE ip='" .$serverip. "';");
 		if (!$result) common_log($servername." - ELASTIC not updated!");
-
 		sleep(10);
 	}
 	$connection1->close();
-
 }
 
 function runtask($task, $serverip) {
@@ -99,6 +86,7 @@ function runtask($task, $serverip) {
 
 function la($connection, $serverip) {
 	global $servername;
+	$la = "* * *";
 	if (ssh2_auth_pubkey_file($connection, 'root', '/root/.ssh/id_rsa.pub', '/root/.ssh/id_rsa', '')) {
 		$la = substr(strrchr(ssh2_return($connection, "/usr/bin/uptime"),":"),1);
 		$la1 = intval(array_map("trim",explode(",",$la))[0]);
@@ -106,36 +94,28 @@ function la($connection, $serverip) {
 		if ($la1 < ($core/2)) $fontcolor = "<font color=\"green\">";
 		elseif (($la1 >= ($core/2)) && ($la1 < ($core * 0.75))) $fontcolor = "<font color=\"#CAC003\">";
 		else $fontcolor = "<font color=\"red\">";
-	} else {
-		$fontcolor = "<font color=\"red\">";
-		$la = "* * *";
-	}
+	} else $fontcolor = "<font color=\"red\">";
 	return "<a title=\"Click to show processes\" 
 			   href=\"https://" .$servername. "/index.php?task=top&serverip=" .$serverip. "\"
 			   target=\"_blank\">" .$fontcolor. "<b>" .$la. "</b></font>\n</a>";
 }
 
 function rep($connection, $serverip) {
+	$data = array();
+	$data["Slave_SQL_Running"] = $data["Slave_IO_Running"] = $data["Seconds_Behind_Master"] = "***";
 	if (ssh2_auth_pubkey_file($connection, 'root', '/root/.ssh/id_rsa.pub', '/root/.ssh/id_rsa', '')) {
 		$str = ssh2_return($connection, "mysql -e 'show slave status\G'");
-		$data = array();
 		foreach (explode("\n", $str) as $cLine) {
 			list ($cKey, $cValue) = explode(':', $cLine, 2);
 			$data[trim($cKey)] = trim($cValue);
 		}
-
 	    if ($data["Slave_SQL_Running"] == "Yes") $sqlfontcolor = "<font color=\"green\">";
 	    else $sqlfontcolor = "<font color=\"red\">";
-
 	    if ($data["Slave_IO_Running"] == "Yes") $iofontcolor = "<font color=\"green\">";
 	    else $iofontcolor = "<font color=\"red\">";
-
 	    if ($data["Seconds_Behind_Master"] == 0) $deltafontcolor = "<font color=\"green\">";
 	    else $deltafontcolor = "<font color=\"red\">";
-	} else {
-		$sql = $io = $delta = "***";
-		$sqlfontcolor = $iofontcolor = $deltafontcolor = "<font color=\"red\">";
-	}
+	} else $sqlfontcolor = $iofontcolor = $deltafontcolor = "<font color=\"red\">";
     return "<a title=\"Click to restart replication\" 
     		   href=\"#\" 
     		   onclick=\"myAjax(\'" .$serverip. "\')\">
@@ -146,16 +126,16 @@ function rep($connection, $serverip) {
 
 function err500($connection, $serverip) {
 	global $servername;
+	$str = "***";
 	if (ssh2_auth_pubkey_file($connection, 'root', '/root/.ssh/id_rsa.pub', '/root/.ssh/id_rsa', ''))
-	    $str = trim(preg_replace('/\s+/', ' ', ssh2_return($connection, "cat /var/log/500err.log")));
-    else
-    	$str = "***";
+	    $str = trim(ssh2_return($connection, "cat /var/log/500err.log"));
     return "<a title=\"Click to show 500 errors\" 
     		 href=https://". $servername. "/index.php?task=500err&serverip=" .$serverip. " 
     		 target=\"_blank\">" .$str. "\n</a>";
 }
 
 function elastic($connection, $serverip) {
+	$str = "***";
 	if (ssh2_auth_pubkey_file($connection, 'root', '/root/.ssh/id_rsa.pub', '/root/.ssh/id_rsa', '')) {
 		$str = ssh2_return($connection, "date1=\$((\$(date +'%s%N') / 1000000));
 										 curl -sS -o /dev/null -XGET http://`/sbin/ifconfig eth1 | 
@@ -164,16 +144,9 @@ function elastic($connection, $serverip) {
 										 awk '{ print $1}'`:9200/_cluster/health?pretty;
 										 date2=\$((\$(date +'%s%N') / 1000000));
 										 echo -n \$((\$date2-\$date1));");
-		if ( $str == "Timeout" ) {
-			$str = "***";
-			$fontcolor = "<font color=\"red\">";
-		} else {
-			$fontcolor = "<font color=\"green\">";
-		}
-	} else {
-		$str = "***";
-		$fontcolor = "<font color=\"red\">";
-	}
+		if ( $str == "Timeout" ) $fontcolor = "<font color=\"red\">";
+		else $fontcolor = "<font color=\"green\">";
+	} else $fontcolor = "<font color=\"red\">";
 	return $fontcolor.$str. "</font>";
 }
 
@@ -186,13 +159,11 @@ function sigHandler($signo) {
 			common_log("SIGTERM stop");
 			break;
 		}
-		
 		case SIGPIPE: {
 			$stop_server = true;
 			common_log("SIGPIPE stop");
 			break;
 		}
-
 		default: {
 			break;
 		}
