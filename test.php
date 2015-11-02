8a3e478dc34cba822eeeb86f4b47b2da08c3c35b
Himaster <himaster@inbox.ru>
2015-11-02 17:21:24 +0200

    add test.php

---
+++ test.php
@@ -0,0 +1,19 @@
+<?php
+
+$dbconnection = new mysqli("188.138.234.38", "mymon", "eiGo7iek", "mymon") or die($dbconnection->connect_errno."\n");
+$result = $dbconnection->query("SELECT * FROM `mymon`.`servers`;") or die($connection->error());
+$serverinfo = array();
+echo "<table class=\"table\">";
+while ($row_user = $result->fetch_assoc()) {
+?>
+	<tr>
+		<td><input value="<?php echo $row_user['id'] ?>"></td>
+		<td><input value="<?php echo $row_user['ip'] ?>"></td>
+		<td><input value="<?php echo $row_user['servername'] ?>"></td>
+		<td><input value="<?php echo $row_user['db'] ?>"></td>
+		<td><input value="<?php echo $row_user['err'] ?>"></td>
+		<td><input value="<?php echo $row_user['el'] ?>"></td>
+	</tr>
+<?php
+}
+echo ("</table>");
