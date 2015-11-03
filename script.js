20b7143e0167b7815751f5f4cef2b99e36fd5cae
Himaster <himaster@inbox.ru>
2015-11-03 14:27:34 +0200

    fix

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ function show(serverip, server, task){
             $("#" + server + "_" + task).html(html);
         },
         error: function(){
-            $("#" + server + "_" + task).html("Error");
+            $("#" + server + "_" + task).addClass('forceTimeout');
         }
     });
 }
