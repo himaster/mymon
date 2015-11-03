3bc724ad83bed3e7f2bcc1d24c1014f3ac4f4062
Himaster <himaster@inbox.ru>
2015-11-03 13:12:02 +0200

    test

--- test.php
+++ test.php
@@ -1,20 +1,3 @@
-<html>
-	<head>
-		<title></title>
-		<style>
-			#a1 {
-				-moz-transition: color 2s 2s ease;
-				-o-transition: color 2s 2s ease;
-				-webkit-transition: color 2s 2s ease;
-				color:#222;
-			}
-			 
-			#a1:hover {
-			 	color: #1FA2E1;
-			}
-		</style>
-	</head>
-	<body>
-		<div id="a1" name="a1">Test</dev>
-	</body>
-</html>
+<?php
+
+echo "<script>console.log('console log: Test. ');</script>";
