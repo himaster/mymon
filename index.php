adbcb448ad1688a10b3e1173481da39c036535ac
Himaster <himaster@inbox.ru>
2015-11-03 13:32:05 +0200

    test

--- index.php
+++ index.php
@@ -21,8 +21,7 @@ if (isset($_COOKIE["mymon"])) {
 			$connection = ssh2_connect($_GET["serverip"], 22);
 			start:
 			if (!ssh2_auth_pubkey_file($connection, 'root', 'id_rsa.pub', 'id_rsa', '')) {
-   				echo "<script>console.log('console log: Reconnecting to ".$_GET["serverip"]."');</script>";
-   				die(1);
+   				die("<script>console.log('console log: Reconnecting to ".$_GET["serverip"]."');</script>");
    				goto start;
 			}
 		}
