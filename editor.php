1a37609c93cdea29393171b3ef262845e67eced9
Himaster <himaster@inbox.ru>
2015-11-12 18:04:11 +0200

    test

--- editor.php
+++ editor.php
@@ -11,22 +11,3 @@
 echo "Test"; die();
 ?>
 
-<?php
-$dbconnection = new mysqli("188.138.234.38", "mymon", "eiGo7iek", "mymon") or die($dbconnection->connect_errno."\n");
-$result = $dbconnection->query("SELECT `id`, `servername`, `ip`, `db`, `mysql`, `err`, `el` FROM `mymon`.`stats`;") or die($dbconnection->error);
-$serverinfo = array();
-
-while ($row_user = $result->fetch_assoc()) {
-?>
-	<tr>
-
-		<td><input id="<?php echo 'name_'.trim($row_user['servername']; ?>" type="text" value="<?php echo trim($row_user['servername']); ?>" onchange="javascript: alert(this); "></td>
-		<td><input id="<?php echo 'ip_'.trim($row_user['servername']; ?>" type="text" value="<?php echo trim($row_user['ip']); ?>"></td>
-		<td><input id="<?php echo 'db_'.trim($row_user['servername']; ?>" type="checkbox" <?php if (trim($row_user['db']) == 1) echo "checked"; ?>></td>
-		<td><input id="<?php echo 'mysql_'.trim($row_user['servername']; ?>" type="checkbox" <?php if (trim($row_user['mysql']) == 1) echo "checked"; ?>></td>
-		<td><input id="<?php echo 'err_'.trim($row_user['servername']; ?>" type="checkbox" <?php if (trim($row_user['err']) == 1) echo "checked"; ?>></td>
-		<td><input id="<?php echo 'el_'.trim($row_user['servername']; ?>" type="checkbox" <?php if (trim($row_user['el']) == 1) echo "checked"; ?>></td>
-	</tr>
-<?php
-}
-echo ("</table>");
