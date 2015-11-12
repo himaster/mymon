9a83db04a498ef7d297f08dbdd2b9b9ed452b305
Himaster <himaster@inbox.ru>
2015-11-12 18:22:28 +0200

    test

--- editor.php
+++ editor.php
@@ -9,10 +9,11 @@
     </tr>
 
 <?php
+echo "Test";die();
 $dbconnection = new mysqli("188.138.234.38", "mymon", "eiGo7iek", "mymon") or die($dbconnection->connect_errno."\n");
 $result = $dbconnection->query("SELECT `id`, `servername`, `ip`, `db`, `mysql`, `err`, `el` FROM `mymon`.`stats`;") or die($dbconnection->error);
 $serverinfo = array();
-echo "Test";die();
+
 while ($row_user = $result->fetch_assoc()) {
 	echo "<tr>";
 	echo "<td><input id=\"name_".trim($row_user['servername']."\" type=\"text\" value=\"".trim($row_user['servername'])."\" onchange=\"javascript: alert(this); \"></td>";
