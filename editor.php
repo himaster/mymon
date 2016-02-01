9397098c375b229bef122d8a872b0588b3e3bac6
Himaster <himaster@inbox.ru>
2016-02-01 15:43:44 +0200

    editor php->html

--- editor.php
+++ editor.php
@@ -3,20 +3,20 @@
 </div>
 <table class="main_table">
 	<col span="5">
-		<tr class="title">
-	        <td>Server</td>
-	        <td>IP Address</td>
-	        <td>Role</td>
-	        <td>Replication</td>
-	        <td>500s</td>
-	        <td>Elastic</td>
-	        <td>Locks</td>
-	    </tr>
+	<tr class="title">
+        <td>Server</td>
+        <td>IP Address</td>
+        <td>Role</td>
+        <td>Replication</td>
+        <td>500s</td>
+        <td>Elastic</td>
+        <td>Locks</td>
+    </tr>
 <?php
 	$dbconnection = new mysqli("188.138.234.38", "mymon", "eiGo7iek", "mymon") or die($dbconnection->connect_errno."\n");
 	$result = $dbconnection->query("SELECT `id`, `servername`, `ip`, `role`, `db`, `mysql`, `err`, `el` FROM `mymon`.`stats` ORDER BY `servername`;") or die($dbconnection->error);
 	while ($row_user = $result->fetch_assoc()) { ?>
-		<tr>
+	<tr>
 		<td><input id="<?php echo trim($row_user['servername']) ?>^servername" type="text" value="<?php echo trim($row_user['servername']) ?>" onchange="javascript: editor(this.id, this.value);"></td>
 		<td><input id="<?php echo trim($row_user['servername']) ?>^ip" type="text" value="<?php echo trim($row_user['ip']) ?>" onchange="javascript: editor(this.id, this.value); "></td>
 		<td><input id="<?php echo trim($row_user['servername']) ?>^role" type="text" value="<?php echo trim($row_user['role']) ?>" onchange="javascript: editor(this.id, this.value); "></td>
@@ -24,7 +24,5 @@
 		<td><input id="<?php echo trim($row_user['servername']) ?>^err" type="checkbox" <?php if (trim($row_user['err']) == 1) echo "checked "; ?> onchange="javascript: editor(this.id, this.checked);"></td>
 		<td><input id="<?php echo trim($row_user['servername']) ?>^el" type="checkbox" <?php if (trim($row_user['el']) == 1) echo "checked "; ?> onchange="javascript: editor(this.id, this.checked);"></td>
 		<td><input id="<?php echo trim($row_user['servername']) ?>^mysql" type="checkbox" <?php if (trim($row_user['mysql']) == 1) echo "checked "; ?> onchange="javascript: editor(this.id, this.checked);"></td>
-		</tr>
-<?php	}
-	echo ("</table>");
-?>
+	</tr>
+</table>
