ba56e233b6e8cea925419b2b1c4f375cf5afec46
Himaster <himaster@inbox.ru>
2016-02-03 12:20:27 +0200

    test

--- test.php
+++ test.php
@@ -14,5 +14,5 @@ $mongo = new MongoDB($c, $db);
 $mongodb_info = $mongo->command(array('serverStatus'=>true));
 
 $datetime2 = new DateTime($val2);
-echo($datetime1->diff($datetime2));
+var_dump($datetime1->diff($datetime2));
 echo("Conn: ".$mongodb_info['connections']['current'];);
