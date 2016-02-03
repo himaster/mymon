1bcf754d42105bf0cff76e0fbd2ecc20b6f01dfe
Himaster <himaster@inbox.ru>
2016-02-03 19:08:20 +0200

    test

--- menu.php
+++ menu.php
@@ -28,26 +28,28 @@ if ($result->num_rows == 1) { ?>
 				<input type="text" id="email" name="email" value="<?php echo $umail ?>">
 			</div>
 			<p>
-			<div class="simple_div">
-				<input type="checkbox" id="la" <?php if ($ula == 1) echo "checked "; ?> >
-			</div>
-			<div class="simple_div">
-				<input type="checkbox" id="rep" <?php if ($urep == 1) echo "checked "; ?> >
-			</div>
-			<div class="simple_div">
-				<input type="checkbox" id="loc" <?php if ($uloc == 1) echo "checked "; ?> >
-			</div>
-			<div class="simple_div">
-				<input type="checkbox" id="500" <?php if ($u500 == 1) echo "checked "; ?> >
-			</div>
-			<div class="simple_div">
-				<input type="checkbox" id="el" <?php if ($uel == 1) echo "checked "; ?> >
-			</div>
-			<div class="simple_div">
-				<input type="checkbox" id="mon" <?php if ($umon == 1) echo "checked "; ?> >
-			</div>
-			<div class="simple_div">
-				<input type="checkbox" id="red" <?php if ($ured == 1) echo "checked "; ?> >
+			<div id="simple_div">
+				<div class="simple_div">
+					<input type="checkbox" id="la" <?php if ($ula == 1) echo "checked "; ?> >
+				</div>
+				<div class="simple_div">
+					<input type="checkbox" id="rep" <?php if ($urep == 1) echo "checked "; ?> >
+				</div>
+				<div class="simple_div">
+					<input type="checkbox" id="loc" <?php if ($uloc == 1) echo "checked "; ?> >
+				</div>
+				<div class="simple_div">
+					<input type="checkbox" id="500" <?php if ($u500 == 1) echo "checked "; ?> >
+				</div>
+				<div class="simple_div">
+					<input type="checkbox" id="el" <?php if ($uel == 1) echo "checked "; ?> >
+				</div>
+				<div class="simple_div">
+					<input type="checkbox" id="mon" <?php if ($umon == 1) echo "checked "; ?> >
+				</div>
+				<div class="simple_div">
+					<input type="checkbox" id="red" <?php if ($ured == 1) echo "checked "; ?> >
+				</div>
 			</div>
 			<p>
 			<div id="submit_div">
