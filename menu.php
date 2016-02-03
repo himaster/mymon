a846ec337b22d114965e7735737b600fa4c08124
Himaster <himaster@inbox.ru>
2016-02-03 21:36:31 +0200

    test

--- menu.php
+++ menu.php
@@ -30,25 +30,25 @@ if ($result->num_rows == 1) { ?>
 			<p>
 			<div id="simple_div"> simple view:
 				<div style="display: inline-block;">
-					<input type="checkbox" id="la" <?php if ($ula == 1) echo "checked "; ?> >
+					<input type="checkbox" id="la" name="la" <?php if ($ula == 1) echo "checked "; ?> >
 				</div>
 				<div style="display: inline-block;">
-					<input type="checkbox" id="rep" <?php if ($urep == 1) echo "checked "; ?> >
+					<input type="checkbox" id="rep" name="rep" <?php if ($urep == 1) echo "checked "; ?> >
 				</div>
 				<div style="display: inline-block;">
-					<input type="checkbox" id="loc" <?php if ($uloc == 1) echo "checked "; ?> >
+					<input type="checkbox" id="loc" name="loc" <?php if ($uloc == 1) echo "checked "; ?> >
 				</div>
 				<div style="display: inline-block;">
-					<input type="checkbox" id="500" <?php if ($u500 == 1) echo "checked "; ?> >
+					<input type="checkbox" id="500" name="500" <?php if ($u500 == 1) echo "checked "; ?> >
 				</div>
 				<div style="display: inline-block;">
-					<input type="checkbox" id="el" <?php if ($uel == 1) echo "checked "; ?> >
+					<input type="checkbox" id="el" name="el" <?php if ($uel == 1) echo "checked "; ?> >
 				</div>
 				<div style="display: inline-block;">
-					<input type="checkbox" id="mon" <?php if ($umon == 1) echo "checked "; ?> >
+					<input type="checkbox" id="mon" name="mon" <?php if ($umon == 1) echo "checked "; ?> >
 				</div>
 				<div style="display: inline-block;">
-					<input type="checkbox" id="red" <?php if ($ured == 1) echo "checked "; ?> >
+					<input type="checkbox" id="red" name="red" <?php if ($ured == 1) echo "checked "; ?> >
 				</div>
 			</div>
 			<p>
