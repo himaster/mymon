3e68da83f7c410cb0a43306f0e3a11b7f5a48e64
Himaster <himaster@inbox.ru>
2016-02-03 14:57:47 +0200

    fix

--- collector.php
+++ collector.php
@@ -168,7 +168,7 @@ function mongo($connection, $serverip) {
 
 function redis($connection, $serverip) {
 	$str = ssh2_return($connection, "date1=\$((\$(date +'%s%N') / 1000000));
-									 mongo admin --quiet --eval 'printjson(db.serverStatus().connections.current)';
+									 mongo admin --quiet --eval 'printjson(db.serverStatus().connections.current) 1>/dev/null';
 									 date2=\$((\$(date +'%s%N') / 1000000));
 									 echo -n \$((\$date2-\$date1));");
 	if ( $str == "Timeout" ) $fontcolor = "<font color=\"red\">";
