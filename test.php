2d93d60c856a7264a1239fbf196c85f8da9d506c
Himaster <himaster@inbox.ru>
2016-02-03 12:08:58 +0200

    test

--- test.php
+++ test.php
@@ -13,6 +13,6 @@ $c  = new MongoClient($hosts); // connect
 $mongo = new MongoDB($c, $db);
 
 $mongodb_info = $mongo->command(array('serverStatus'=>true));
-$mongodb_connections = $mongodb_info['connections'];
+$mongodb_connections = $mongodb_info['connections']['current'];
 
 die($mongodb_connections);
