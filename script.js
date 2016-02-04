12948185da3c170a3a28bdfc6683739d872f5450
Himaster <himaster@inbox.ru>
2016-02-04 16:25:11 +0200

    fix

--- script.js
+++ script.js
@@ -46,10 +46,10 @@ function show() {
                 $("#" + item['servername'] + "_mongo").html(item['mongo']);
                 $("#" + item['servername'] + "_redis").html(item['redis']);
                 if (!item['message']) { 
-                    $("#messagebox").html("")};
+                    $("#messagebox").html("");
                     //document.getElementById('messagebox').style.display = 'none';
                 } else {
-                    //$("#messagebox").html(item['message'])};
+                    $("#messagebox").html(item['message']);
                     //document.getElementById('messagebox').style.display = 'block';
                 }
 
