97310c0af2f3f3204f4157d6c4ad8cdfba52df82
Himaster <himaster@inbox.ru>
2016-02-04 17:11:20 +0200

    test

--- script.js
+++ script.js
@@ -48,10 +48,10 @@ function show() {
                 if (!item['message']) { 
                     $("#message").html("");
                     document.getElementById('messagebox').style.display = 'none';
-                    document.getElementById('main_table').style.display = 'table';
+                    //document.getElementById('main_table').style.display = 'table';
                 } else {
                     $("#message").html(item['message']);
-                    document.getElementById('main_table').style.display = 'none';
+                    //document.getElementById('main_table').style.display = 'none';
                     document.getElementById('messagebox').style.display = 'block';
                 }
 
