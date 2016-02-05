b5db3a5fa5251b66ede0c86a016f055711a0dc4d
Himaster <himaster@inbox.ru>
2016-02-05 15:10:04 +0200

    test

--- test.php
+++ test.php
@@ -7,14 +7,15 @@
 		<select name="uselect">
 		<?php
 			require_once "functions.php";
-			$connection = new mysqli("188.138.234.38", "mymon", "eiGo7iek", "mymon") or die($connection->connect_errno."\n");
-			$result = $dbconnection->query("SELECT id, login  FROM `mymon`.`users` WHERE approvied='1'") or die($dbconnection->error);
+			$dbconnection = new mysqli("188.138.234.38", "mymon", "eiGo7iek", "mymon") or die($dbconnection->connect_errno."\n");
+			$result = $dbconnection->query("SELECT `id`, `login`  FROM `mymon`.`users` WHERE approvied='1'") or die($dbconnection->error);
 			while($array = $result->fetch_assoc()) {
 				$uid = $array["id"];
 				$ulogin = $array["login"];
 		  		echo "<option value=\"$uid\">$ulogin</option>";
 			}
 		?>
+		</select>
 	</form>
 </body>
 </html>
