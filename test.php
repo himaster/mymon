aeb01354d7538972dcee53c095d8356001206324
Himaster <himaster@inbox.ru>
2016-02-05 15:07:27 +0200

    test

--- test.php
+++ test.php
@@ -1,12 +1,20 @@
-<?php
-
-#require_once "functions.php";
-$connection = new mysqli("188.138.234.38", "mymon", "eiGo7iek", "mymon") or die($connection->connect_errno."\n");
-
-$str = ssh2_return($connection, "date1=\$((\$(date +'%s%N') / 1000000));
-									 mongo admin --quiet --eval 'printjson(db.serverStatus().connections.current)';
-									 date2=\$((\$(date +'%s%N') / 1000000));
-									 echo -n \$((\$date2-\$date1));");
-	if ( $str == "Timeout" ) $fontcolor = "<font color=\"red\">";
-	else $fontcolor = "<font color=\"green\">";
-	return $fontcolor.$str. "</font>";
+<html>
+<head>
+	<title>Test</title>
+</head>
+<body>
+	<form name="form" method="post" action="/test.php">
+		<select name="uselect">
+		<?php
+			require_once "functions.php";
+			$connection = new mysqli("188.138.234.38", "mymon", "eiGo7iek", "mymon") or die($connection->connect_errno."\n");
+			$result = $dbconnection->query("SELECT id, login  FROM `mymon`.`users` WHERE approvied='1'") or die($dbconnection->error);
+			while($array = $result->fetch_assoc()) {
+				$uid = $array["id"];
+				$ulogin = $array["login"];
+		  		echo "<option value=\"$uid\">$ulogin</option>";
+			}
+		?>
+	</form>
+</body>
+</html>
