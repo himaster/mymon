4f7119d79764c145b6641ec08e8e3f0c6f1f0b01
Himaster <himaster@inbox.ru>
2016-02-05 16:59:34 +0200

    test

--- index.php
+++ index.php
@@ -154,6 +154,34 @@ if (isset($_COOKIE["mymon"])) {
 				mail($result->fetch_assoc()['email'], "Mymon registration", $msg, $headers);
 				echo "<p>Профиль успешно обновлен";
 				break;
+				
+			case "sendmsg":
+				if (isset($_POST['submit'])) {
+					$umessage = no_injection($_POST['umessage']);
+					$ulogins = array_merge(array(0 => ' '), $_POST['uselect']);
+					$str = implode(" ,", $ulogins);
+					$query = "INSERT INTO `mymon`.`messages` (`message`, `sender`, `receiver`) VALUES ('$umessage', '$uid', '$str');";
+					$result = $dbconnection->query($query) or die($dbconnection->error);
+					echo "Message sent.";
+					exit;
+				}
+				$result = $dbconnection->query("SELECT `id`, `login`  FROM `mymon`.`users` WHERE approvied='1'") or die($dbconnection->error);
+				?>	
+				<form method="post">
+					<textarea name="umessage"></textarea>
+					<p><select multiple name="uselect[]">
+						<?php
+							while($array = $result->fetch_assoc()) {
+								$uid = $array["id"];
+								$ulogin = $array["login"];
+								echo "<option value=\"$uid\">$ulogin</option>";
+							}
+						?>
+					</select>
+					<p><input type="submit" name="submit" value="send">
+				</form>
+				<?php
+				break;
 
 			case "msgred":
 				$result = $dbconnection->query("UPDATE `mymon`.`messages` SET isRead = 1 WHERE receiver LIKE '%,".$uid."%' and isRead = 0 and isDeleted = 0 LIMIT 1;") or die($dbconnection->error());
