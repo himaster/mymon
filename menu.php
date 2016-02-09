204c62f68b990041c0a6449383d75ff2a7c37803
Himaster <himaster@inbox.ru>
2016-02-09 16:08:39 +0200

    Title fix

--- menu.php
+++ menu.php
@@ -24,63 +24,60 @@ if ($result->num_rows == 1) { ?>
 	</a>
 </div>
 <div id="my_div" class="menu">
-	
-		<div id="menu_title"><b>Profile</b></div>
-		<form action="reg.php" method="POST">
-			<p>IP: <?php echo $_SERVER['REMOTE_ADDR'] ?>
-			<p>username:<input class="username" type="text" id="login" name="login" value="<?php echo $login ?>" readonly>
-			<p><a href="javascript: toggle_visibility('password_div');
-								 toggle_visibility('password2_div');
-								 toggle_visibility('email_div');
-								 toggle_visibility('submit_div');
-								 toggle_visibility('simple_div');
-								 ">
-				Edit profile
-			</a>
+	<b>Profile</b>
+	<form action="reg.php" method="POST">
+		<p>IP: <?php echo $_SERVER['REMOTE_ADDR'] ?>
+		<p>username:<input class="username" type="text" id="login" name="login" value="<?php echo $login ?>" readonly>
+		<p><a href="javascript: toggle_visibility('password_div');
+							 toggle_visibility('password2_div');
+							 toggle_visibility('email_div');
+							 toggle_visibility('submit_div');
+							 toggle_visibility('simple_div');
+							 ">
+			Edit profile
+		</a>
+		<p>
+		<div id="password_div"> password:
+			<input type="password" id="password" name="password">
+		</div>
+		<p>
+		<div id="password2_div"> repeat:
+			<input type="password" id="password2" name="password2">
+		</div>
+		<p>
+		<div id="email_div"> e-mail:
+			<input type="text" id="email" name="email" value="<?php echo $umail ?>">
+		</div>
+		<p>
+		<div class="simple_div" id="simple_div" name="simple_div"> columns:
 			<p>
-			<div id="password_div"> password:
-				<input type="password" id="password" name="password">
+			<div style="display: inline-block;">
+				* 1<input type="checkbox" id="la" name="la" <?php if ($ula == 1) echo "checked "; ?> >
 			</div>
-			<p>
-			<div id="password2_div"> repeat:
-				<input type="password" id="password2" name="password2">
+			<div style="display: inline-block;">
+				2<input type="checkbox" id="rep" name="rep" <?php if ($urep == 1) echo "checked "; ?> >
 			</div>
-			<p>
-			<div id="email_div"> e-mail:
-				<input type="text" id="email" name="email" value="<?php echo $umail ?>">
+			<div style="display: inline-block;">
+				3<input type="checkbox" id="loc" name="loc" <?php if ($uloc == 1) echo "checked "; ?> >
 			</div>
-			<p>
-			<div class="simple_div" id="simple_div" name="simple_div"> columns:
-				<p>
-				<div style="display: inline-block;">
-					* 1<input type="checkbox" id="la" name="la" <?php if ($ula == 1) echo "checked "; ?> >
-				</div>
-				<div style="display: inline-block;">
-					2<input type="checkbox" id="rep" name="rep" <?php if ($urep == 1) echo "checked "; ?> >
-				</div>
-				<div style="display: inline-block;">
-					3<input type="checkbox" id="loc" name="loc" <?php if ($uloc == 1) echo "checked "; ?> >
-				</div>
-				<div style="display: inline-block;">
-					4<input type="checkbox" id="500" name="500" <?php if ($u500 == 1) echo "checked "; ?> >
-				</div>
-				<div style="display: inline-block;">
-					5<input type="checkbox" id="el" name="el" <?php if ($uel == 1) echo "checked "; ?> >
-				</div>
-				<div style="display: inline-block;">
-					6<input type="checkbox" id="mon" name="mon" <?php if ($umon == 1) echo "checked "; ?> >
-				</div>
-				<div style="display: inline-block;">
-					7<input type="checkbox" id="red" name="red" <?php if ($ured == 1) echo "checked "; ?> >
-				</div>
+			<div style="display: inline-block;">
+				4<input type="checkbox" id="500" name="500" <?php if ($u500 == 1) echo "checked "; ?> >
 			</div>
-			<p>
-			<div id="submit_div">
-				<input type="submit" id="submit_edit" name="submit_edit" value="send">
+			<div style="display: inline-block;">
+				5<input type="checkbox" id="el" name="el" <?php if ($uel == 1) echo "checked "; ?> >
 			</div>
-			
-			<p>
-			<p><a href="https://<?php echo $_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']) ?>/?task=exit">logout</a>
-		</form>
-
+			<div style="display: inline-block;">
+				6<input type="checkbox" id="mon" name="mon" <?php if ($umon == 1) echo "checked "; ?> >
+			</div>
+			<div style="display: inline-block;">
+				7<input type="checkbox" id="red" name="red" <?php if ($ured == 1) echo "checked "; ?> >
+			</div>
+		</div>
+		<p>
+		<div id="submit_div">
+			<input type="submit" id="submit_edit" name="submit_edit" value="send">
+		</div>		
+		<p>
+		<p><a href="https://<?php echo $_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']) ?>/?task=exit">logout</a>
+	</form>
 </div>
