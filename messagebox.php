42a6dce653c9611182092288409f3d15d0a6aa16
Himaster <himaster@inbox.ru>
2016-02-09 17:39:47 +0200

    test

--- messagebox.php
+++ messagebox.php
@@ -1,6 +1,5 @@
-<div>
+<div id="messagebox" class="messagebox">
 	<h5>System message</h5>
-	<div id="messagebox" class="messagebox">
     <div id="message" class="box effect2 new"></div>
     <a class="mbclose" href="javascript: mbclose();">x</a>
     <a class="mbclose2" href="javascript: mbclose();">Close</a>
