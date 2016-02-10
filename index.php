d5cdb2566aefd5dc505ee827b2fb528cd27f6e4b
Himaster <himaster@inbox.ru>
2016-02-10 12:07:24 +0200

    msg test

--- index.php
+++ index.php
@@ -160,6 +160,7 @@ if (isset($_COOKIE["mymon"])) {
 				#$ulogins = array_merge(array(0 => ','), $_POST['uselect']);
 				$ulogins = $_POST['uselect'];
 				$str = implode(",", $ulogins);
+				die($str);
 				$query = "INSERT INTO `mymon`.`messages` (`message`, `sender`, `receiver`) VALUES ('$umessage', '$uid', '$str');";
 				$result1 = $dbconnection->query($query) or die("Error occured".$dbconnection->error);
 				echo "Message sent.";
