f991873672edbd992a726989ab3e318728434a90
Himaster <himaster@inbox.ru>
2016-02-10 14:49:41 +0200

    close button fix

--- messagebox.php
+++ messagebox.php
@@ -2,5 +2,5 @@
     <div id="message" class="box effect2 new"></div>
     <div id="message_title" class="message_title"></div>
     <a class="mbclose" href="javascript: mbclose();">x</a>
-    <a class="mbclose2" href="javascript: mbclose();">Close</a>
+    <a class="mbclose2" href="javascript: mbclose();"><button>Close</button></a>
 </div>
