50ec5048d88f3d408b40743ef9a1ef04620aef85
Himaster <himaster@inbox.ru>
2016-02-10 12:44:51 +0200

    test

--- index.php
+++ index.php
@@ -157,8 +157,9 @@ if (isset($_COOKIE["mymon"])) {
 
 			case "sendmsg":
 				$umessage = no_injection($_POST['umessage']);
-				$ulogins = ",".implode(",", $_POST['uselect']).",";
-				$result = $dbconnection->query("INSERT INTO `mymon`.`messages` (`message`, `sender`, `receiver`) VALUES ('$umessage', '$uid', '$ulogins');") or die("Error occured".$dbconnection->error);
+				foreach ($_POST['uselect'] as $ulogin) {
+					$result = $dbconnection->query("INSERT INTO `mymon`.`messages` (`message`, `sender`, `receiver`) VALUES ('$umessage', '$uid', '$ulogin');") or die("Error occured".$dbconnection->error);
+				}
 				echo "Message sent.";
 				break;
 
