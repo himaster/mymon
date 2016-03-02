bda58732c766068fda74a9a35ef43e7cd0ab8246
Himaster <himaster@inbox.ru>
2016-03-02 12:47:54 +0200

    test

--- index.php
+++ index.php
@@ -131,19 +131,15 @@ if (isset($_COOKIE["mymon"])) {
 				break;
 
 			case 'getdata':
-				$rows=[];
+				$rows=array();
 				$result = $dbconnection->query("SELECT `id`, UNIX_TIMESTAMP(`timestamp`) as `timestamp`, `servername`, `la`, `rep`, `500`, `elastic`, `locks`, `mongo`, `redis` FROM `mymon`.`stats`") or die($dbconnection->error());
 				while($array = $result->fetch_assoc()) {
 					$rows[] = $array;
 				}
 				$result = $dbconnection->query("SELECT `messages`.`id`, UNIX_TIMESTAMP(`messages`.`timestamp`) as `timestamp`, `messages`.`message`, `users`.`login` FROM `mymon`.`messages` JOIN `users` WHERE `messages`.`sender` = `users`.`id` AND `receiver` = '$uid' AND isRead = 0 AND isDeleted = 0 LIMIT 1;") or die($dbconnection->error());
 				if(mysqli_num_rows($result)>0){
-					$test = $result->fetch_assoc();
-					var_dump($test);
-					$rows[] = $test;
+					$rows[] = $result->fetch_assoc();
 				}
-				#var_dump($rows);
-				#die();
 				header("Content-Type: application/json; charset=utf-8 ");
 				echo json_encode($rows);
 				break;
