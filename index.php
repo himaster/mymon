1c4b423f049485a44ae5cf3036753c62513393e6
Himaster <himaster@inbox.ru>
2016-03-02 12:20:16 +0200

    debug

--- index.php
+++ index.php
@@ -139,7 +139,7 @@ if (isset($_COOKIE["mymon"])) {
 				$result = $dbconnection->query("SELECT `messages`.`id`, UNIX_TIMESTAMP(`messages`.`timestamp`) as `timestamp`, `messages`.`message`, `users`.`login` FROM `mymon`.`messages` JOIN `users` WHERE `messages`.`sender` = `users`.`id` AND `receiver` = '$uid' AND isRead = 0 AND isDeleted = 0 LIMIT 1;") or die($dbconnection->error());
 				if(mysqli_num_rows($result)>0){
 					$test = $result->fetch_assoc();
-					echo $test;
+					echo "<pre>".$test."</pre>";
 					$rows[] = $test;
 				}
 				#header("Content-Type: application/json; charset=utf-8 ");
