78af2c5ee44aee0d05f608ce7f841a88a1085ab1
Himaster <himaster@inbox.ru>
2016-03-02 15:52:38 +0200

    test

--- collector.php
+++ collector.php
@@ -109,14 +109,14 @@ function rep($connection, $serverip) {
     	$sql = "&#10003;";
     } else {
     	$sqlfontcolor = "<font color=\"red\">";
-    	$sql = "x<script type='javascript'>notify('Replication SQL problem');</script>";
+    	$sql = "x<script type='text/javascript'>notify('Replication SQL problem');</script>";
     }
     if ($data["Slave_IO_Running"] == "Yes") {
     	$iofontcolor = "<font color=\"green\">";
     	$io = "&#10003;";
     } else {
     	$iofontcolor = "<font color=\"red\">";
-    	$io = "x<script type='javascript'>notify('Replication IO problem');</script>";
+    	$io = "x<script type='text/javascript'>notify('Replication IO problem');</script>";
     }
     if ($data["Seconds_Behind_Master"] == "0") $deltafontcolor = "<font color=\"green\">";
     else $deltafontcolor = "<font color=\"red\">";
@@ -142,7 +142,7 @@ function elastic($connection, $serverip) {
 									 curl -sS -o /dev/null -XGET http://`ip -f inet addr show eth1 | grep -Po 'inet \K[\d.]+'`:9200/_cluster/health?pretty;
 									 date2=\$((\$(date +'%s%N') / 1000000));
 									 echo -n \$((\$date2-\$date1));");
-	if ( $str == "Timeout" ) $fontcolor = "<font color=\"red\"><script type='javascript'>notify('Elastic problem');</script>";
+	if ( $str == "Timeout" ) $fontcolor = "<font color=\"red\"><script type='text/javascript'>notify('Elastic problem');</script>";
 	else $fontcolor = "<font color=\"green\">";
 	return $fontcolor.$str. "<font size=\"1\"> ms</font></font>";
 }
@@ -151,7 +151,7 @@ function locks($connection, $serverip) {
 	$locked = trim(ssh2_return($connection, "mysql -Ne \"SELECT info FROM INFORMATION_SCHEMA.PROCESSLIST WHERE state LIKE '%lock%' AND time > 30\" | wc -l"));
 	$conns  = trim(ssh2_return($connection, "mysql -Nse \"SHOW STATUS WHERE variable_name = 'Threads_connected'\" | awk '{print $2}'"));
     if (($locked == "0") and ($conns < "1000")) $fontcolor = "<font color=\"green\">";
-    else $fontcolor = "<font color=\"red\"><script type='javascript'>notify('DB locks detected');</script>";
+    else $fontcolor = "<font color=\"red\"><script type='text/javascript'>notify('DB locks detected');</script>";
     return $fontcolor.$conns. " / " .$locked. "</font>";
 }
 
@@ -160,7 +160,7 @@ function mongo($connection, $serverip) {
 									 mongo admin --quiet --eval 'printjson(db.serverStatus().connections.current)' 1>/dev/null;
 									 date2=\$((\$(date +'%s%N') / 1000000));
 									 echo -n \$((\$date2-\$date1));");
-	if ( $str == "Timeout" ) $fontcolor = "<font color=\"red\"><script type='javascript'>notify('Mongo problem');</script>";
+	if ( $str == "Timeout" ) $fontcolor = "<font color=\"red\"><script type='text/javascript'>notify('Mongo problem');</script>";
 	else $fontcolor = "<font color=\"green\">";
 	return $fontcolor.$str. "<font size=\"1\"> ms</font></font>";
 }
@@ -170,7 +170,7 @@ function redis($connection, $serverip) {
 									 redis-cli info 1>/dev/null;
 									 date2=\$((\$(date +'%s%N') / 1000000));
 									 echo -n \$((\$date2-\$date1));");
-	if ( $str == "Timeout" ) $fontcolor = "<font color=\"red\"><script type='javascript'>notify('Redis problem');</script>";
+	if ( $str == "Timeout" ) $fontcolor = "<font color=\"red\"><script type='text/javascript'>notify('Redis problem');</script>";
 	else $fontcolor = "<font color=\"green\">";
 	return $fontcolor.$str. "<font size=\"1\"> ms</font></font>";
 }
