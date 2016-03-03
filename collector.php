2926a1c8b0aedd7c08af0a63ac7b652a0fb2369d
Himaster <himaster@inbox.ru>
2016-03-03 12:20:03 +0200

    fix

--- collector.php
+++ collector.php
@@ -101,11 +101,11 @@ function rep($connection, $serverip) {
 	$data = array();
 	$str = ssh2_return($connection, "printf %s \"$(mysql -e 'show slave status\G' | awk 'FNR>1')\"");
 	foreach (explode("\n", $str) as $cLine) {
-		if (strpos($cLine, 'ERROR') !== false) {
-			return "<font color=\"red\">Mysql stopped</font>";
-		}
 		list($cKey, $cValue) = explode(':', $cLine, 2);
 		$data[trim($cKey)] = trim($cValue);
+	}
+	if (!array_key_exists("Slave_SQL_Running", $data)) {
+		return "<font color=\"red\">Mysql stopped</font>";
 	}
     if ($data["Slave_SQL_Running"] == "Yes") {
     	$sqlfontcolor = "<font color=\"green\">";
@@ -118,7 +118,7 @@ function rep($connection, $serverip) {
     	$iofontcolor = "<font color=\"green\">";
     	$io = "&#10003;";
     } else {
-    	$iofontcolor = "<script type=\"text/javascript\">new_mes = notify(\"Replication IO problem\"); setTimeout(new_mes.close.bind(new_mes), 2000);</script><font color=\"red\">";
+    	$iofontcolor = "<script type=\"text/javascript\">new_mes = notify(\"Replication IO problem\");setTimeout(new_mes.close.bind(new_mes), 2000);</script><font color=\"red\">";
     	$io = "x";
     }
     if ($data["Seconds_Behind_Master"] == "0") $deltafontcolor = "<font color=\"green\">";
@@ -147,7 +147,7 @@ function elastic($connection, $serverip) {
 									 curl -sS -o /dev/null -XGET http://`ip -f inet addr show eth1 | grep -Po 'inet \K[\d.]+'`:9200/_cluster/health?pretty;
 									 date2=\$((\$(date +'%s%N') / 1000000));
 									 echo -n \$((\$date2-\$date1));");
-	if ( $str == "Timeout" ) $fontcolor = "<script type=\"text/javascript\">new_mes = notify(\"Elastic problem\"); setTimeout(new_mes.close.bind(new_mes), 2000);</script><font color=\"red\">";
+	if ( $str == "Timeout" ) $fontcolor = "<script type=\"text/javascript\">new_mes = notify(\"Elastic problem\");setTimeout(new_mes.close.bind(new_mes), 2000);</script><font color=\"red\">";
 	else $fontcolor = "<font color=\"green\">";
 
 	return $fontcolor.$str. "<font size=\"1\"> ms</font></font>";
@@ -167,7 +167,7 @@ function mongo($connection, $serverip) {
 									 mongo admin --quiet --eval 'printjson(db.serverStatus().connections.current)' 1>/dev/null;
 									 date2=\$((\$(date +'%s%N') / 1000000));
 									 echo -n \$((\$date2-\$date1));");
-	if ( $str == "Timeout" ) $fontcolor = "<script type=\"text/javascript\">new_mes = notify(\"Mongo problem\"); setTimeout(new_mes.close.bind(new_mes), 2000);</script><font color=\"red\">";
+	if ( $str == "Timeout" ) $fontcolor = "<script type=\"text/javascript\">new_mes = notify(\"Mongo problem\");setTimeout(new_mes.close.bind(new_mes), 2000);</script><font color=\"red\">";
 	else $fontcolor = "<font color=\"green\">";
 
 	return $fontcolor.$str. "<font size=\"1\"> ms</font></font>";
@@ -178,7 +178,7 @@ function redis($connection, $serverip) {
 									 redis-cli info 1>/dev/null;
 									 date2=\$((\$(date +'%s%N') / 1000000));
 									 echo -n \$((\$date2-\$date1));");
-	if ( $str == "Timeout" ) $fontcolor = "<script type=\"text/javascript\">new_mes = notify(\"Redis problem\"); setTimeout(new_mes.close.bind(new_mes), 2000);</script><font color=\"red\">";
+	if ( $str == "Timeout" ) $fontcolor = "<script type=\"text/javascript\">new_mes = notify(\"Redis problem\");setTimeout(new_mes.close.bind(new_mes), 2000);</script><font color=\"red\">";
 	else $fontcolor = "<font color=\"green\">";
 
 	return $fontcolor.$str. "<font size=\"1\"> ms</font></font>";
