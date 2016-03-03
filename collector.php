454daea33e6d34869d885c8b66662f1065d6df2c
Himaster <himaster@inbox.ru>
2016-03-03 16:14:09 +0200

    suppress "ssh2_connect(): Unable to connect to" errors

--- collector.php
+++ collector.php
@@ -39,8 +39,8 @@ function child_() {
 	$ssh_conname = "ssh_".$servername;
 	common_log($servername. " - started.");
 	start:
-	$$ssh_conname = ssh2_connect($serverip, 22);
-	if ((!$$ssh_conname) or (!ssh2_auth_pubkey_file($$ssh_conname, 'root', '/var/www/netbox.co/mymon/id_rsa.pub', '/var/www/netbox.co/mymon/id_rsa', ''))) {
+	#$$ssh_conname = ssh2_connect($serverip, 22);
+	if ((!$$ssh_conname = @ssh2_connect($serverip, 22)) or (!@ssh2_auth_pubkey_file($$ssh_conname, 'root', '/var/www/netbox.co/mymon/id_rsa.pub', '/var/www/netbox.co/mymon/id_rsa', ''))) {
 		common_log($servername." - retry #".$i++.".");
 		sleep(1);
 		if ($i < $retry_num) goto start;
