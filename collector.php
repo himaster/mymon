bd0b245cded5fb91f5c950ec30a55bfa6dd0d227
Himaster <himaster@inbox.ru>
2016-03-04 15:40:57 +0200

    css fix

--- collector.php
+++ collector.php
@@ -156,7 +156,7 @@ function elastic($connection, $serverip) {
 function locks($connection, $serverip) {
 	$locked = trim(ssh2_return($connection, "mysql -Ne \"SELECT info FROM INFORMATION_SCHEMA.PROCESSLIST WHERE state LIKE '%lock%' AND time > 30\" | wc -l"));
 	$conns  = trim(ssh2_return($connection, "mysql -Nse \"SHOW STATUS WHERE variable_name = 'Threads_connected'\" | awk '{print $2}'"));
-    if (($locked == "0") and ($conns < "1000")) $fontcolor = "<font color=\"green\">";
+    if (($locked == "0") and ($conns < "5000")) $fontcolor = "<font color=\"green\">";
     else $fontcolor = "<script type=\"text/javascript\">notify(\"DB locks\");</script><font color=\"red\">";
 
     return $fontcolor.$conns. " / " .$locked. "</font>";
