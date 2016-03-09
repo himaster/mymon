3f404dc8d89f831d4ee540b9dedd21d04a3446e1
Himaster <himaster@inbox.ru>
2016-03-09 21:07:10 +0200

    test

--- script.js
+++ script.js
@@ -243,7 +243,7 @@ $(document).ready(function() {
         } else {
             console.log(window.animation[1]);
             $(this).animate(window.animation[1], {duration: 200, easing: 'swing'});
-            setTimeout($(this).removeAttr("style"),500);
+            setTimeout(function() { $(this).removeAttr("style"); },500);
             expanded = false;
         }
     });
