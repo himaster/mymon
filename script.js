a8f09ddf606333277cc52c358b5c74b53f67e941
Himaster <himaster@inbox.ru>
2016-03-09 12:58:22 +0200

    js fix

--- script.js
+++ script.js
@@ -237,8 +237,8 @@ $(function() {
     var expanded = false;
     if (detectmob()) var animation = [{'top' : '5px'}, {'top' : '-3px'}];
     else var animation = [{'left' : '5px'}, {'left' : '-3px'}];
-    console.log(animation);
     $('#left_button').click(function() {
+        console.log(animation);
         if (!expanded) {
             $(this).animate(animation[0], {duration : 400});
             expanded = true;
