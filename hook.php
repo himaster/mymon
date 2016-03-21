afbf597795b100d753ac544eca7a48434a2dc8fc
Himaster <himaster@inbox.ru>
2016-03-21 11:47:16 +0200

    hook added

---
+++ hook.php
@@ -0,0 +1,5 @@
+<?php
+    putenv('PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin:/var/www/confogen/');
+    exec('cd /var/www/netbox.co/mymon/ && git pull https://github.com/himaster/mymon.git master 2>&1', $output);
+    print_r($output);
+?>
