e52d0cacb8ade6f3400d526fafcdc49f8b30f9c8
Himaster <himaster@inbox.ru>
2016-03-22 13:05:28 +0200

    fix

--- collector.php
+++ collector.php
@@ -126,7 +126,7 @@ function rep($connection, $serverip) {
     return "<select onChange=\"javascript: eval( this.value )();\"><option value=\"$\">
     		   SQL: $sqlfontcolor<b>$sql</b></font> 
     		   IO: $iofontcolor<b>$io</b></font> 
-    		   &#916;: $deltafontcolor<b>$data[\'Seconds_Behind_Master\']</b></font>\n</option>
+    		   &#916;: $deltafontcolor<b>" .$data["Seconds_Behind_Master"]. "</b></font>\n</option>
     		   <option value=\"if(confirm(\'Want to repair replication?\')) replica_repair(\'" .$serverip. "\');\">repair</optin>
     		   <option value=\"if(confirm(\'Want to restart replication?\')) myAjax(\'" .$serverip. "\');\">reset</optin>
     		</select>";
