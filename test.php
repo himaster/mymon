dfd458756d3b207dc797cdddb8d1a26c8682bf21
Himaster <himaster@inbox.ru>
2016-03-23 12:36:56 +0200

    test

--- test.php
+++ test.php
@@ -5,6 +5,7 @@ function notify( $message ) {
     if ($notify == 1) return "<script type=\"text/javascript\">notify(\"$message\");</script>";
 }
 
+$notify = 1;
 $sqlfontcolor = notify("Replication SQL problem")."<font color=\"red\">";
 
 echo $sqlfontcolor;
