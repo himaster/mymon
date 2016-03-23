0521874946ea9e7554d5fe2e06331c63b5d34575
Himaster <himaster@inbox.ru>
2016-03-23 20:38:29 +0200

    test

--- test.php
+++ test.php
@@ -1,12 +1,6 @@
 <?php
 
-function notify( $message ) {
-    global $notify;
-    echo "\$notify=".$notify;
-    if ($notify == 1) return "<script type=\"text/javascript\">notify(\"$message\");</script>";
-}
-
-$notify = 0;
-$sqlfontcolor = notify("Replication SQL problem")."<font color=\"red\">";
-
-echo $sqlfontcolor;
+$la = 0.96;
+$core = 12;
+$percent = intval($la/$core*100);
+echo $percent;
