ad226d878f480c4f4d725a5a7abcf85087e73e23
Himaster <himaster@inbox.ru>
2016-03-25 17:03:49 +0200

    test

--- messagebox.php
+++ messagebox.php
@@ -1,4 +1,5 @@
 <div id="messagebox" class="messagebox">
+	<div id="fade" class="fading"></div>
     <div id="message" class="box effect2 new"></div>
     <div id="message_title" class="message_title"></div>
     <a class="mbclose" href="javascript: mbclose();">x</a>
