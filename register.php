e3e53c5e94ff3aee31eeb3ae1062af8ad0d45c64
Himaster <himaster@inbox.ru>
2016-03-26 21:08:57 +0200

    test decapitalize

--- register.php
+++ register.php
@@ -6,11 +6,11 @@
 		<form action="reg.php" method="POST">
 			<tr>
 				<td>Имя</td>
-				<td><input type="text" name="login" ></td>
+				<td><input type="text" name="login" autocapitalize="none"></td>
 			</tr>
 			<tr>
 				<td>Пароль</td>
-				<td><input type="password" name="password" ></td>
+				<td><input type="password" name="password"></td>
 			</tr>
 			<tr>
 				<td>Еще разок</td>
@@ -18,7 +18,7 @@
 			</tr>
 			<tr>
 				<td>Email</td>
-				<td><input type="text" name="email"></td>
+				<td><input type="text" name="email" autocapitalize="none"></td>
 			</tr>
 			<tr>
 				<p><td colspan="2"><input type="submit" value="OK" name="submit" ></td>
