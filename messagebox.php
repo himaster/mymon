1370d16c8f6887a9c8e93340952199181686daf1
Himaster <himaster@inbox.ru>
2016-03-28 15:25:56 +0300

    test

--- messagebox.php
+++ messagebox.php
@@ -3,5 +3,6 @@
     <div id="message" class="box effect2 new"></div>
     <div id="message_title" class="message_title"></div>
     <a class="mbclose" href="javascript: mbclose();">x</a>
-    <a class="mbclose2" href="javascript: mbclose();"><button>Close</button></a>
+    <input type="button" class="mbclose2" onclick="javascript: mbclose();" />
+    <!--<a class="mbclose2" href="javascript: mbclose();"><button>Close</button></a> -->
 </div>
