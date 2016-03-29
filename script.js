39b1367fa2366474d8afb0c1f10948c2a39ad85b
Himaster <himaster@inbox.ru>
2016-03-29 15:59:59 +0300

    test

--- script.js
+++ script.js
@@ -90,7 +90,7 @@ function show() {
                 document.getElementById("messagebox").style.display = "none";
             } else {
                 $("#message").html(json.msg.message);
-                $("#message_title").html('<h3>Message from ' + json.msg.login + '</h3>');
+                $("#message_title").html('<h4>Message from ' + json.msg.login + '</h4>');
                 if (document.getElementById("messagebox").style.display !== "block") {
                     notify("New message from " + json.msg.login + "\n" + json.msg.message, 10000);
                 }
