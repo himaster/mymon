a3ebaa61f67eed9e2d3b797749f7f0c62fd43e87
Himaster <himaster@inbox.ru>
2016-04-14 11:53:04 +0300

    editor css fix

--- editor.php
+++ editor.php
@@ -4,24 +4,24 @@
 <table class="main_table">
 	<col span="5">
 	<tr class="title">
-        <td>Server</td>
-        <td>IP address</td>
-        <td>Role</td>
-        <td>Replication</td>
-        <td>Locks</td>
-        <td>500s</td>
-        <td>Elastic</td>
-        <td>Mongo</td>
-        <td>Redis</td>
+        <td class="serv">Server</td>
+        <td class="la">IP address</td>
+        <td class="role">Role</td>
+        <td class="rep">Replication</td>
+        <td class="loc">Locks</td>
+        <td class="500">500s</td>
+        <td class="el">Elastic</td>
+        <td class="mon">Mongo</td>
+        <td class="red">Redis</td>
     </tr>
 <?php
 	$dbconnection = new mysqli("188.138.234.38", "mymon", "eiGo7iek", "mymon") or die($dbconnection->connect_errno."\n");
 	$result = $dbconnection->query("SELECT `id`, `servername`, `ip`, `role`, `db`, `mysql`, `err`, `el`, `mon`, `red` FROM `mymon`.`stats` ORDER BY `servername`;") or die($dbconnection->error);
 	while ($row_user = $result->fetch_assoc()) { ?>
 	<tr>
-		<td><input id="<?php echo trim($row_user['servername']) ?>^servername" type="text" value="<?php echo trim($row_user['servername']) ?>" onchange="javascript: editor(this.id, this.value);"></td>
+		<td class="serv"><input id="<?php echo trim($row_user['servername']) ?>^servername" type="text" value="<?php echo trim($row_user['servername']) ?>" onchange="javascript: editor(this.id, this.value);"></td>
 		<td class="la"><input id="<?php echo trim($row_user['servername']) ?>^ip" type="text" value="<?php echo trim($row_user['ip']) ?>" onchange="javascript: editor(this.id, this.value); "></td>
-		<td><input id="<?php echo trim($row_user['servername']) ?>^role" type="text" value="<?php echo trim($row_user['role']) ?>" onchange="javascript: editor(this.id, this.value); "></td>
+		<td class="role"><input id="<?php echo trim($row_user['servername']) ?>^role" type="text" value="<?php echo trim($row_user['role']) ?>" onchange="javascript: editor(this.id, this.value); "></td>
 		<td class="rep"><input id="<?php echo trim($row_user['servername']) ?>^db" type="checkbox" <?php if (trim($row_user['db']) == 1) echo "checked "; ?> onchange="javascript: editor(this.id, this.checked);"></td>
 		<td class="loc"><input id="<?php echo trim($row_user['servername']) ?>^mysql" type="checkbox" <?php if (trim($row_user['mysql']) == 1) echo "checked "; ?> onchange="javascript: editor(this.id, this.checked);"></td>
 		<td class="500"><input id="<?php echo trim($row_user['servername']) ?>^err" type="checkbox" <?php if (trim($row_user['err']) == 1) echo "checked "; ?> onchange="javascript: editor(this.id, this.checked);"></td>
