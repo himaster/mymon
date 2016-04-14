26f8af81bf5b5a3ba9aa6e65b4e370098a8fdff8
Himaster <himaster@inbox.ru>
2016-04-14 16:45:09 +0300

    test

--- users.php
+++ users.php
@@ -25,7 +25,7 @@
 			<td class="serv"><input id="<?php echo trim($row_user['login']) ?>^uid" type="text" value="<?php echo trim($row_user['id']) ?>" onchange="javascript: editor(this.id, this.value);"></td>
 			<td class="la"><input id="<?php echo trim($row_user['login']) ?>^login" type="text" value="<?php echo trim($row_user['login']) ?>" onchange="javascript: editor(this.id, this.value); "></td>
 			<td class="role"><input id="<?php echo trim($row_user['login']) ?>^email" type="text" value="<?php echo trim($row_user['email']) ?>" onchange="javascript: editor(this.id, this.value); "></td>
-			<td class="rep"><input id="<?php echo trim($row_user['login']) ?>^role" type="text" value="<?php echo trim($row_user['role']) ?>" onchange="javascript: editor(this.id, this.checked);"></td>
+			<td class="rep"><input id="<?php echo trim($row_user['login']) ?>^role" type="text" value="<?php echo trim($row_user['roles']) ?>" onchange="javascript: editor(this.id, this.checked);"></td>
 			<td class="loc"><input id="<?php echo trim($row_user['login']) ?>^approvied" type="checkbox" <?php if (trim($row_user['approvied']) == 1) echo "checked "; ?> onchange="javascript: editor(this.id, this.checked);"></td>
 		</tr>
 	<?php } ?>
