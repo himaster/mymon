535720bd85ac8677bc63eed478b634ce5ae24094
Himaster <himaster@inbox.ru>
2016-04-18 16:33:51 +0300

    test

--- js/testscript.js
+++ js/testscript.js
@@ -1,4 +1,4 @@
 $( window ).scroll(function() {
   var scrolled = window.pageYOffset || document.documentElement.scrollTop;
   document.getElementById('showScroll').innerHTML = scrolled + 'px';
-}
+});
