0bb92187b444ac45d4ed57f9ba8a978d60fcd5f8
Himaster <himaster@inbox.ru>
2016-04-18 15:44:00 +0300

    Revert "test"
    This reverts commit 6dad9821aafc58729bfe36f8f471e51f79a89f12.

--- js/script.js
+++ js/script.js
@@ -328,17 +328,6 @@ $(document).ready(function() {
     $("input[name=password]").keyup(function() {
         $("input[name=password2]").val($(this).val());
     });
-    $('.scrollable').pullToRefresh({
-        callback: function() {
-            var def = $.Deferred();
-
-            setTimeout(function() {
-                def.resolve();      
-            }, 3000); 
-
-            return def.promise();
-        }
-    });
 });
 
 
