f67fb77acdaab62fd9edc81c0ea7ca330e821c43
Himaster <himaster@inbox.ru>
2016-04-20 12:28:29 +0300

    test

--- users.php
+++ users.php
@@ -6,7 +6,7 @@
 <table class="main_table table-striped">
 	<col span="5">
 	<tr class="title">
-        <td class="loc">UID</td>
+        <td class="loc" width="60px">UID</td>
         <td>Login</td>
         <td>Email</td>
         <td>Role</td>
