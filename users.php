c8be80fd0a509bbb765bd1661473ff6066f9d3de
Himaster <himaster@inbox.ru>
2016-04-22 12:18:10 +0300

    test

--- users.php
+++ users.php
@@ -30,7 +30,7 @@
 		$user_roles = explode(",", $row_user['roles']);
 		?>
 		<tr>
-			<td><input id="<?php echo trim($row_user['login']) ?>^uid" type="text" value="<?php echo trim($row_user['id']) ?>" onchange="javascript: users_editor(this.id, this.value);"></td>
+			<td class="uid"><input id="<?php echo trim($row_user['login']) ?>^uid" type="text" value="<?php echo trim($row_user['id']) ?>" onchange="javascript: users_editor(this.id, this.value);"></td>
 			<td><input id="<?php echo trim($row_user['login']) ?>^login" type="text" value="<?php echo trim($row_user['login']) ?>" onchange="javascript: users_editor(this.id, this.value); "></td>
 			<td><input id="<?php echo trim($row_user['login']) ?>^email" type="text" value="<?php echo trim($row_user['email']) ?>" onchange="javascript: users_editor(this.id, this.value); "></td>
 			<td><select id="<?php echo trim($row_user['login']) ?>^role" multiple onchange="javascript: users_editor(this.id, implode(',', $(this).val())); ">
