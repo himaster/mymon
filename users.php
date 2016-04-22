6bc60fecef044be37aed983da2e48797de4847ac
Himaster <himaster@inbox.ru>
2016-04-22 11:30:52 +0300

    test

--- users.php
+++ users.php
@@ -33,7 +33,7 @@
 			<td><input id="<?php echo trim($row_user['login']) ?>^uid" type="text" value="<?php echo trim($row_user['id']) ?>" onchange="javascript: users_editor(this.id, this.value);"></td>
 			<td><input id="<?php echo trim($row_user['login']) ?>^login" type="text" value="<?php echo trim($row_user['login']) ?>" onchange="javascript: users_editor(this.id, this.value); "></td>
 			<td><input id="<?php echo trim($row_user['login']) ?>^email" type="text" value="<?php echo trim($row_user['email']) ?>" onchange="javascript: users_editor(this.id, this.value); "></td>
-			<td><select id="<?php echo trim($row_user['login']) ?>^role" multiple onchange="javascript: users_editor(this.id, this.value); ">
+			<td><select id="<?php echo trim($row_user['login']) ?>^role" multiple onchange="javascript: console.log(this.id, this.value); ">
 			<?php
 				foreach ($roles as $key => $value) {
 					if (in_array($key, $user_roles)) $selected = "selected='selected'";
