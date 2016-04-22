b9fa2d28224b25d4a1eb66e57a3d5b7e8fa60064
Himaster <himaster@inbox.ru>
2016-04-22 11:04:18 +0300

    test

--- users.php
+++ users.php
@@ -16,6 +16,15 @@
     </tr>
 <?php
 	$dbconnection = new mysqli("188.138.234.38", "mymon", "eiGo7iek", "mymon") or die($dbconnection->connect_errno."\n");
+	$result = $dbconnection->query("SELECT `id`, `name`
+									FROM `mymon`.`roles`") or die($dbconnection->error);
+	while($row = $result->fetch_assoc()){
+    	$roles[$row['id']] = $row;
+	}
+	echo "<pre>";
+	var_dump($roles);
+	echo "</pre>";
+	die();
 	$result = $dbconnection->query("SELECT `id`, `login`, `email`, `approvied`, GROUP_CONCAT(`ur`.`role_id`) AS roles
 									FROM `mymon`.`users` 
 									LEFT JOIN `mymon`.`user_roles` AS `ur` ON (`id` = `ur`.`user_id`)
@@ -25,7 +34,13 @@
 			<td><input id="<?php echo trim($row_user['login']) ?>^uid" type="text" value="<?php echo trim($row_user['id']) ?>" onchange="javascript: users_editor(this.id, this.value);"></td>
 			<td><input id="<?php echo trim($row_user['login']) ?>^login" type="text" value="<?php echo trim($row_user['login']) ?>" onchange="javascript: users_editor(this.id, this.value); "></td>
 			<td><input id="<?php echo trim($row_user['login']) ?>^email" type="text" value="<?php echo trim($row_user['email']) ?>" onchange="javascript: users_editor(this.id, this.value); "></td>
-			<td><input id="<?php echo trim($row_user['login']) ?>^role" type="text" value="<?php echo trim($row_user['roles']) ?>" onchange="javascript: users_editor(this.id, this.value);"></td>
+			<td><select id="<?php echo trim($row_user['login']) ?>^role" multiple onchange="javascript: users_editor(this.id, this.value); ">
+			<?php
+
+			?>
+			</select></td>
+			<!-- <td><input id="<?php echo trim($row_user['login']) ?>^role" type="text" value="<?php echo trim($row_user['roles']) ?>" onchange="javascript: users_editor(this.id, this.value);"></td> -->
+	
 			<td><input id="<?php echo trim($row_user['login']) ?>^approvied" type="checkbox" <?php if (trim($row_user['approvied']) == 1) echo "checked "; ?> onchange="javascript: users_editor(this.id, this.checked);"></td>
 		</tr>
 	<?php } ?>
