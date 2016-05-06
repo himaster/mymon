4cfa64ea4d9697fbb2d25366741a98d7f21725fe
Himaster <himaster@inbox.ru>
2016-05-06 18:48:35 +0300

    PSR2 fix

--- functions.php
+++ functions.php
@@ -2,43 +2,54 @@
 
 $hostname = "mymon.pkwteile.de";
 
-function no_injection( $str='' ) { 
-    $str = stripslashes( $str ); 
-    $str = trim( $str ); 
-    $str = htmlspecialchars( $str ); 
-    return $str; 
-} 
-
-function ssh2_return( $connection, $query ) {
-	$stream = ssh2_exec( $connection, $query );
-    $error_stream = ssh2_fetch_stream( $stream, SSH2_STREAM_STDERR );
-	stream_set_blocking( $error_stream, TRUE );
-	$error_output = stream_get_contents( $error_stream );
-	stream_set_blocking( $stream, TRUE );
-	$output = stream_get_contents( $stream );
-	if (!empty($error_output)) return "Timeout";
-	else return $output;
+function no_injection($str = '')
+{
+    $str = stripslashes($str);
+    $str = trim($str);
+    $str = htmlspecialchars($str);
+    return $str;
 }
 
-function common_log($logmsg) {
-	$date = date('Y-m-d H:i:s (T)');
-	$f = fopen('/var/log/mymon/common.txt', 'a');
-	if (!empty($f)) {
-		fwrite($f, "$date: PID:".getmypid()."  $logmsg\r\n");
-		fclose($f);
-	}
+function ssh2_return($connection, $query)
+{
+    $stream = ssh2_exec($connection, $query);
+    $error_stream = ssh2_fetch_stream($stream, SSH2_STREAM_STDERR);
+    stream_set_blocking($error_stream, true);
+    $error_output = stream_get_contents($error_stream);
+    stream_set_blocking($stream, true);
+    $output = stream_get_contents($stream);
+    if (!empty($error_output)) {
+        return "Timeout";
+    } else {
+        return $output;
+    }
 }
 
-function console_log($data) {
-    if (is_array($data)) $output = "<script>console.log('console log: " .implode(',', $data). "');</script>";
-    else $output = "<script>console.log('console log: " .$data. "');</script>";
+function common_log($logmsg)
+{
+    $date = date('Y-m-d H:i:s (T)');
+    $f = fopen('/var/log/mymon/common.txt', 'a');
+    if (!empty($f)) {
+        fwrite($f, "$date: PID:".getmypid()."  $logmsg\r\n");
+        fclose($f);
+    }
+}
+
+function console_log($data)
+{
+    if (is_array($data)) {
+        $output = "<script>console.log('console log: " .implode(',', $data). "');</script>";
+    } else {
+        $output = "<script>console.log('console log: " .$data. "');</script>";
+    }
     echo $output;
 }
 
-function host_scheme() {
-	$isSecure = false;
-	if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') {
-    	$isSecure = true;
-	}
-	return $isSecure ? 'https' : 'http';
+function host_scheme()
+{
+    $isSecure = false;
+    if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') {
+        $isSecure = true;
+    }
+    return $isSecure ? 'https' : 'http';
 }
