cbfe5e3b8873263e28b351efc8caeff855c77fc1
Himaster <himaster@inbox.ru>
2016-05-06 10:36:22 +0300

    PSR2 format fix

--- 500.php
+++ 500.php
@@ -1,18 +1,36 @@
 <?php
-	if (!$connection = ssh2_connect($_GET["serverip"], 22)) {
-		header($_SERVER['SERVER_PROTOCOL'] . ' 501 Internal Server Error', true, 500);
-		die("Connection error!");
-	}
-	ssh2_auth_pubkey_file($connection, 'root', '/var/www/netbox.co/mymon/id_rsa.pub', '/var/www/netbox.co/mymon/id_rsa', '');
+
+/**
+ * 500 File Doc Comment
+ *
+ * @category 500 errors
+ * @package   MyMon
+ * @author    himaster
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     http://mymon.pkwteile.de
+ *
+ */
+
+if (!$connection = ssh2_connect($_GET["serverip"], 22)) {
+    header($_SERVER['SERVER_PROTOCOL'] . ' 501 Internal Server Error', true, 500);
+    die("Connection error!");
+}
+ssh2_auth_pubkey_file($connection, 'root', '/var/www/netbox.co/mymon/id_rsa.pub', '/var/www/netbox.co/mymon/id_rsa', '');
+
 ?>
-	<a href="http://<?php echo $_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']) ?>">
-		<div class="left_button" id="back_button">
-			<img src="images/back.png">
-		</div>
-	</a>
-	<div class="textstyle">
-<?php
-	$str = ssh2_return($connection, "cat /var/log/500.errs");
-	echo nl2br($str);
-?>
+
+<a href="http://<?php echo $_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']) ?>">
+	<div class="left_button" id="back_button">
+		<img src="images/back.png">
 	</div>
+</a>
+<div class="textstyle">
+
+<?php
+
+$str = ssh2_return($connection, "cat /var/log/500.errs");
+echo nl2br($str);
+
+?>
+
+</div>
