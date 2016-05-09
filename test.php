9bda2b59021a9e801fd05255ae1b74cdd0822683
Himaster <himaster@inbox.ru>
2016-05-09 18:52:32 +0300

    test

--- test.php
+++ test.php
@@ -2,8 +2,11 @@
 require_once 'functions.php';
 
 error_reporting(E_ERROR | E_WARNING | E_PARSE);
+
+$docroot  = dirname(__FILE__);
 $serverip = "pkwteile.no-ip.biz";
 $connection = ssh2_connect($serverip, 22);
+ssh2_auth_pubkey_file($connection, 'root', $docroot.'/id_rsa.pub', $docroot.'/id_rsa', '');
 $str = ssh2_return($connection, "printf %s \"$(mysql -e 'show slave status\G' | awk 'FNR>1')\"");
 
 foreach (explode("\n", $str) as $cLine) {
