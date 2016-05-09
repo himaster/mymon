1df71b64666b58ccf3fef0dd6d06431b8dca31df
Himaster <himaster@inbox.ru>
2016-05-09 18:50:26 +0300

    test

--- test.php
+++ test.php
@@ -3,7 +3,7 @@ require_once 'functions.php';
 
 error_reporting(E_ERROR | E_WARNING | E_PARSE);
 $serverip = "pkwteile.no-ip.biz";
-$connection = @ssh2_connect($serverip, 22);
+$connection = ssh2_connect($serverip, 22);
 $str = ssh2_return($connection, "printf %s \"$(mysql -e 'show slave status\G' | awk 'FNR>1')\"");
 
 foreach (explode("\n", $str) as $cLine) {
