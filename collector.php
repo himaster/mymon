e19e0506bb9cf17d18850f9c69dbd62adbdddefe
Himaster <himaster@inbox.ru>
2016-05-17 15:57:28 +0300

    test

--- collector.php
+++ collector.php
@@ -61,7 +61,7 @@ function parent_()
         }
     }
     foreach (botips($connection) as $value) {
-        common_log("Parent: ".var_dump($value));
+        common_log("Parent: ".$value['amount']);
         //$query = "INSERT INTO `mymon`.`botips` (id, ipaddr, amount) VALUES (, ".$value['amount'].", ".$value['ipaddr'].");";
         //common_log("Parent: ".$query);
     }
