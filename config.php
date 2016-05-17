2c52c57868396cb6c5bfef974dbeb2e0658c4911
Himaster <himaster@inbox.ru>
2016-05-17 15:12:20 +0300

    test

--- config.php
+++ config.php
@@ -3,12 +3,16 @@
 error_reporting(E_ALL);
 
 $isSecure = false;
-if ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') || $_SERVER['SERVER_PORT'] == 443) {
+if ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') || (!empty($_SERVER['HTTPS']) && $_SERVER['SERVER_PORT'] == 443)) {
     $isSecure = true;
 }
 $REQUEST_PROTOCOL = $isSecure ? 'https' : 'http';
 
-$hostname = $REQUEST_PROTOCOL.'://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']);
+if (array_key_exists('HTTP_HOST', $_SERVER)) {
+    $hostname = $REQUEST_PROTOCOL.'://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']);
+} else {
+    $hostname = $REQUEST_PROTOCOL.'://mymon.pkwteile.de';
+}
 
 $loglevel = 'debug';
 
