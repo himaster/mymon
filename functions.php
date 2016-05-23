5f5d2e425f6ee79b3ec51ca3432eb348a941fb87
Himaster <himaster@inbox.ru>
2016-05-23 19:04:56 +0300

    slackbot timeout => 5min

--- functions.php
+++ functions.php
@@ -61,7 +61,7 @@ function slackbot($message)
     $starttime = strtotime(date("Y-m-d H:i:s"));
     $lasttime  = strtotime($dbconnection->query("SELECT `timestamp`
                                   FROM `mymon`.`slack_messages`;")->fetch_row()[0]);
-    if ($starttime - $lasttime > 120 and $slackbotlevel == "full") {
+    if ($starttime - $lasttime > 300 and $slackbotlevel == "full") {
         $dbconnection->query("UPDATE `mymon`.`slack_messages` SET `test` = NOT `test`;");
         $channel = "#sys-admins";
         $username = "mymon-bot";
