d9f025eb842bf47a42a6eec060b8268beb4d225f
Himaster <himaster@inbox.ru>
2016-05-30 15:25:11 +0300

    test

--- whois.php
+++ whois.php
@@ -4,6 +4,10 @@ include "backbutton.php";
 exec("whois ".$_GET['ip'], $output);
 echo "<div class='whois'>";
 foreach ($output as $row) {
-    echo $row."<br>";
+    if ((stripos($row, 'netname') !== false)
+        or (stripos($row, 'descr') !== false)
+        or (stripos($row, 'organisation') !== false)) {
+        echo $row."<br>";
+    }
 }
 echo "</div>";
