0a5f3de4b9f0f4ce0858dc652db99087dad98208
Himaster <himaster@inbox.ru>
2016-06-01 17:04:32 +0300

    test

--- test.php
+++ test.php
@@ -5,6 +5,6 @@ require_once "functions.php";
 
 $slackbotlevel = 'full';
 
-echo "isSecure=".$isSecure;
+echo "isSecure='".$isSecure."'\n";
 var_dump($_SERVER['HTTPS']);
 var_dump($_SERVER['SERVER_PORT']);
