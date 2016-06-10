41a46b1365fd51312dbff59cb3ce70b2d6720fb3
Himaster <himaster@inbox.ru>
2016-06-10 14:13:15 +0300

    test

--- botips.php
+++ botips.php
@@ -74,7 +74,7 @@ while ($row_ip = $result->fetch_assoc()) {
                 <input type="button"
                        class="ban_button"
                        value="ban"
-                       onclick="javascript: ban_ip('<?php echo $row_ip['ipaddr']; ?>', $(this).prev('input').val());
+                       onclick="javascript: ban_ip('<?php echo $row_ip['ipaddr']; ?>', '');
                                             $('#ban_<?php echo $row_ip['id']; ?>').addClass('hidden');">
             </div>
         <td>
