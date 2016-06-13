86239ee7965c3bf54855d3810a8b74c32b60032a
Himaster <himaster@inbox.ru>
2016-06-13 16:48:59 +0300

    test

--- build/scripts/undo-201606131505.sql
+++
@@ -1,9 +0,0 @@
--- Fragment begins: 1 --
-
-DROP TABLE `post`;
-
---//
-DELETE FROM changelog
-	                         WHERE change_number = 1
-	                         AND delta_set = 'Main';
--- Fragment ends: 1 --
