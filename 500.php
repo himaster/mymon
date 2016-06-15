7de8c20276b1a749749412d8e6d057d7afc38952
Himaster <himaster@inbox.ru>
2016-06-15 10:51:01 +0300

    test

--- 500.php
+++ 500.php
@@ -30,9 +30,11 @@ if (empty($_GET["page"])) {
 }
 if ($page > 0) {
 	echo "<a href=500.php?page=".($page - 1)."> &lt; </a>";
+} else {
+	echo " ";
 }
-echo $page;
-echo "<a href=500.php?page=".($page + 1).">&gt;</a>";
+echo " -".$page."- ";
+echo "<a href=500.php?page=".($page + 1).">&gt;</a>\n";
 $str = ssh2_return($connection, "tail -n +".($page * 10)." /var/log/500.errs | head -n 11");
 echo nl2br($str);
 
