e425c98bc44c4b33ed692a2926b8e16044ee9669
Himaster <himaster@inbox.ru>
2016-06-15 22:36:28 +0300

    back button title added

--- backbutton.php
+++ backbutton.php
@@ -1,5 +1,5 @@
 <a href="javascript:history.back()">
     <div class="left_button" id="back_button">
-        <img src="images/back.png">
+        <img src="images/back.png" title="Previous page">
     </div>
 </a>
