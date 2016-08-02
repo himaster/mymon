b3bce251ad24ca5f671d2cfbdbff2b2b7e8a2a2b
Himaster <himaster@inbox.ru>
2016-08-02 11:41:04 +0300

    hook test

--- hook.php
+++ hook.php
@@ -1,7 +1,7 @@
 <?php
-    echo ($_POST["ref"]);
+    print_r($_POST["ref"]);
     putenv('PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin:/var/www/confogen/');
-    exec('cd /var/www/netbox.co/mymon/ && git fetch --all && git reset --hard origin/master && git pull origin master 2>&1', $output1);
+    exec('cd /var/www/netbox.co/mymon/ && git fetch --all && git reset --hard origin/master && git pull 2>&1', $output1);
     print_r($output1."<p>");
     exec('cd /var/www/netbox.co/mymon.test/ && git fetch --all && git reset --hard origin/test && git pull origin test 2>&1', $output2);
     print_r($output2);
