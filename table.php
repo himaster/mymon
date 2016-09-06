1868cd871c4040621c5525f91cd7d37c393025d1
Himaster <himaster@inbox.ru>
2016-09-06 11:32:04 +0300

    Alphanumeric sort

--- table.php
+++ table.php
@@ -47,7 +47,8 @@
         include "menu.php";
         include "messagebox.php";
         $result = $dbconnection->query("SELECT `st`.`servername`,
-                                               CAST(`st`.`servername` AS UNSIGNED) AS `servername2`,
+                                               LEFT(`st`.`servername`,PATINDEX('%[0-9]%',`st`.`servername`)-1) AS `servername_alpha`,
+                                               CONVERT(INT, SUBSTRING(`st`.`servername`, PATINDEX('%[0-9]%', `st`.`servername`), LEN(`st`.`servername`))) AS `servername_numeric`,
                                                `st`.`ip`,
                                                `st`.`db`,
                                                `st`.`mysql`,
@@ -60,7 +61,7 @@
                                         JOIN `stats` AS `st`
                                         ON `st`.`role` = `ur`.`role_id`
                                         WHERE `ur`.`user_id` = {$uid}
-                                        ORDER BY `servername2`,`st`.`servername`;") or die($dbconnection->error());
+                                        ORDER BY `servername_alpha`,`servername_numeric`;") or die($dbconnection->error());
     while ($array = $result->fetch_assoc()) {
         $serverip = $array["ip"];
         $server = $array["servername"];
