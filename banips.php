6190fad1261de2a1523fcb56f10cdc839c6b07df
Himaster <himaster@inbox.ru>
2016-09-23 12:26:21 +0300

    added navigation to banips

--- banips.php
+++ banips.php
@@ -2,6 +2,11 @@
 backButton("/");
 ?>
 
+<a href="index.php?task=botips">
+    <div class="left_button" id="left_button2">
+        <img src="images/sql.png" title="Bot IPs">
+    </div>
+</a>
 <a href="index.php?task=slow_querys">
     <div class="left_button" id="left_button2">
         <img src="images/sql.png" title="MySQL slow scripts">
--- botips.php
+++ botips.php
@@ -2,6 +2,11 @@
 backButton("/");
 ?>
 
+<a href="index.php?task=banips">
+    <div class="left_button" id="left_button2">
+        <img src="images/sql.png" title="Banned IPs">
+    </div>
+</a>
 <a href="index.php?task=slow_querys">
     <div class="left_button" id="left_button2">
         <img src="images/sql.png" title="MySQL slow scripts">
