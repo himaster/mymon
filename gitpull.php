ee7e5b3321ec601efff2b0a4743a3a0529e80fc2
Himaster <himaster@inbox.ru>
2016-09-30 18:52:06 +0300

    git pull test

--- gitpull.php
+++ gitpull.php
@@ -1,6 +1,15 @@
 <?php
 
-$tag = $_GET['tag'];
-$ip = $_GET['ip'];
+$repository = $_GET['tag'];
+$serverip = $_GET['ip'];
 
-echo "tag:".$tag.", ip:".$ip;
+
+if (( ! $connection = ssh2_connect($serverip, 22, $ssh_callbacks))
+    or ( ! ssh2_auth_pubkey_file($connection, 'root', $docroot.'/id_rsa.pub', $docroot.'/id_rsa', ''))) {
+    common_log($servername." - retry #".$i++.".");
+	die();
+}
+
+$str = ssh2_return($connection, "cd /home/developer/www/fuel.$repository/ && git pull");
+
+echo $str;
