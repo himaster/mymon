767692b75c6cfbdd06ac27ced9666fd49308e986
Himaster <himaster@inbox.ru>
2016-09-30 18:59:35 +0300

    test

--- gitpull.php
+++ gitpull.php
@@ -5,12 +5,12 @@ $serverip = $_GET['ip'];
 
 
 if (( ! $connection = ssh2_connect($serverip, 22, $ssh_callbacks))
-    or ( ! ssh2_auth_pubkey_file($connection, 'root', $docroot.'/id_rsa.pub', $docroot.'/id_rsa', ''))) {
+    or ( ! ssh2_auth_pubkey_file($connection, 'developer', $docroot.'/id_rsa.pub', $docroot.'/id_rsa', ''))) {
     common_log($servername." - retry #".$i++.".");
 	echo "SSH Connection error!";
 	die();
 }
 
-$str = ssh2_return($connection, "sudo -s -- 'cd /home/developer/www/fuel.".$repository."/; git pull'");
+$str = ssh2_return($connection, "cd /home/developer/www/fuel.".$repository."/; git pull");
 
 echo $str;
