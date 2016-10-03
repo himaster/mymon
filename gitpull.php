f19259a8f9776f8df216e191ef79102c36a0fa63
Himaster <himaster@inbox.ru>
2016-10-03 15:34:49 +0300

    uniq git hash status

--- gitpull.php
+++ gitpull.php
@@ -11,6 +11,6 @@ if (( ! $connection = ssh2_connect($serverip, 22, $ssh_callbacks))
 	die();
 }
 
-$str = ssh2_return($connection, "cd /home/developer/www/fuel.".$repository."/; git pull");
+$str = ssh2_return($connection, "cd /home/developer/www/fuel.".$repository."/ && git pull");
 
 echo $str;
--- gitstatus.php
+++ gitstatus.php
@@ -33,22 +33,45 @@ backButton("/");
     </tr>
 <?php
 $dbconnection = new mysqli("188.138.234.38", "mymon", "eiGo7iek", "mymon") or die($dbconnection->connect_errno."\n");
-$result = $dbconnection->query("SELECT servername, ip, master_repo, test_repo FROM `stats` WHERE git=1;") or die($dbconnection->error);
+$result = $dbconnection->query("SELECT `st`.`servername`,
+                                       `st`.`ip`,
+                                       `st`.`master_repo`,
+                                       `st`.`test_repo`,
+                                       IF(`st`.`master_repo`=`master_temp`.`master_repo`,'0','1') AS `master_uniq`,
+                                       IF(`st`.`test_repo`=`test_temp`.`test_repo`,'0','1') AS `test_uniq`
+                                       FROM `stats` AS `st`
+                                JOIN
+                                    (SELECT `test_repo`, count(*) AS `count`
+                                     FROM `stats`
+                                     WHERE `test_repo` IS NOT NULL
+                                     GROUP BY `test_repo`
+                                     ORDER BY `count`
+                                     DESC
+                                     LIMIT 1) AS `test_temp`
+                                JOIN
+                                    (SELECT `master_repo`, count(*) AS `count`
+                                     FROM `stats`
+                                     WHERE `master_repo` IS NOT NULL
+                                     GROUP BY `master_repo`
+                                     ORDER BY `count`
+                                     DESC
+                                     LIMIT 1) AS `master_temp`
+                                WHERE `st`.`test_repo` IS NOT NULL AND git=1;") or die($dbconnection->error);
 while ($row_ip = $result->fetch_assoc()) {
 ?>
     <tr>
         <td class="hostname">
-            <?php echo trim($row_ip['servername']);?>
+            <?php echo trim($row_ip['st.servername']);?>
         </td>
         <td class="git_master">
-            <?php if ($isAdmin){ echo "<a href=# onClick=\"gitpull('".$row_ip['ip']."', 'prod')\">";}
-                  echo trim($row_ip['master_repo']);
+            <?php if ($isAdmin){ echo "<a href=# onClick=\"gitpull('".$row_ip['st.ip']."', 'prod')\">";}
+                  echo trim($row_ip['st.master_repo']);
                   if ($isAdmin){ echo "</a>";}
             ?>
         </td>
         <td class="git_test">
-            <?php if ($isAdmin){ echo "<a href=# onClick=\"gitpull('".$row_ip['ip']."', 'dev')\">";}
-                  echo trim($row_ip['test_repo']);
+            <?php if ($isAdmin){ echo "<a href=# onClick=\"gitpull('".$row_ip['st.ip']."', 'dev')\">";}
+                  echo trim($row_ip['st.test_repo']);
                   if ($isAdmin){ echo "</a>";}
             ?>
         <td>
