ff3465c11fba892d73986cc2af4eec267b163b2d
Himaster <himaster@inbox.ru>
2016-10-14 18:57:45 +0300

    confirm test

--- confirm.php
+++ confirm.php
@@ -1,5 +1,7 @@
 <?php
 
+var_dump($_GET);
+die();
 $login = no_injection($_GET["username"]);
 $result = $dbconnection->query("SELECT id FROM $database.`users` WHERE login ='{$login}' LIMIT 1;") or die($dbconnection->error);
 $uid = $result->fetch_assoc()['id'];
