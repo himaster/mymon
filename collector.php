d6a34fb15cbb0ff35af4aa9eacb86aeb5fe42ddb
Himaster <himaster@inbox.ru>
2016-11-08 18:23:12 +0200

    test

--- collector.php
+++ collector.php
@@ -171,7 +171,7 @@ function child_()
     $result = $$mysql_conname->query($query);
     if ($servername == "cdn") {
         ob_start();
-        var_dump($errs);
+        var_dump($query);
         $res = ob_get_clean();
         common_log($servername." - ".$res);
     }
